f9a1edb0a2e605b59bd09bce9cbcfc302556dfe8
Miyuki Koshiba <m.koshiba@everyleaf.com>
2023-05-08 23:17:36 +0900

    refactor

--- programs/20230508.rb
+++ programs/20230508.rb
@@ -1,14 +1,14 @@
-#Peter de Jong Attractor
+#Peter de Jong Attractor 漸化式
 def setup
   createCanvas(800, 800)
   background(0)
 
-  translate(200, 400)
-  stroke(30, 150, 160, 80)
+  translate(600, 400)
+  stroke(250, 70, 130, 80)
   attractor
 
-  translate(400, 0)
-  stroke(250, 70, 130, 80)
+  translate(-400, 0)
+  stroke(30, 150, 160, 80)
   attractor
 end
 
@@ -25,7 +25,7 @@ def attractor
     xx = Math.sin(a*y) - Math.cos(a*x)*c
     yy = Math.sin(b*x) - Math.cos(b*y)*d
 
-    point(-xx*scale, -yy*scale)
+    point(-xx*scale, yy*scale)
     x = xx
     y = yy
   end
