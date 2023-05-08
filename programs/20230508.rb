5e519ba61619d64b6af4087c0613e4b2310e1d9a
Miyuki Koshiba <m.koshiba@everyleaf.com>
2023-05-08 22:31:59 +0900

    Refactor

--- programs/20230508.rb
+++ programs/20230508.rb
@@ -1,8 +1,18 @@
 #Peter de Jong Attractor
 def setup
-  w=800
-  createCanvas(w, w)
+  createCanvas(800, 800)
   background(0)
+
+  translate(200, 400)
+  stroke(30, 150, 160, 80)
+  attractor
+
+  translate(400, 0)
+  stroke(250, 70, 130, 80)
+  attractor
+end
+
+def attractor
   scale = 120
   x = 0.1
   y = 0.1
@@ -11,24 +21,11 @@ def setup
   c = 2
   d = 1
 
-  translate(w/4, w/2)
-  stroke(30, 150, 160, 80)
   (1..300000).each do | i |
     xx = Math.sin(a*y) - Math.cos(a*x)*c
     yy = Math.sin(b*x) - Math.cos(b*y)*d
 
-    point(-xx * scale, -yy*scale)
-    x = xx
-    y = yy
-  end
-
-  translate((w/4)*2, 0)
-  stroke(250, 70, 130, 80)
-  (1..300000).each do | i |
-    xx = Math.sin(a*y) - Math.cos(a*x)*c
-    yy = Math.sin(b*x) - Math.cos(b*y)*d
-
-    point(-xx * scale, -yy*scale)
+    point(-xx*scale, -yy*scale)
     x = xx
     y = yy
   end
