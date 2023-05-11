cae41a73969c61b150412738b39e71bbd2c07080
Miyuki Koshiba <m.koshiba@everyleaf.com>
2023-05-11 16:24:07 +0900

    扇を回転させる

--- programs/20230513.rb
+++ programs/20230513.rb
@@ -1,6 +1,6 @@
 $base = 80
 $split = 9
-$colors = ["#ba083d", "#444444", "#a9a7ad"]
+$colors = ["#ba083d", "#444444", "#a9a7ad", "#444444", "#a9a7ad"]
 def setup
   createCanvas($base * $split, $base * $split)
   angleMode(DEGREES)
@@ -15,10 +15,29 @@ def draw
   while x < width do
     y = d / 2
     while y < width do
-      c = rand(0..2)
+      c = rand(0..4)
       fill($colors[c])
-      #ellipse(x, y, d, d)
-      arc(x - d / 2, y - d / 2, d * 2, d * 2, 0, 90)
+      case c
+      when 0
+        ellipse(x, y, d, d)
+      when 1
+        push
+        translate(0, d)
+        arc(x - d / 2, y - d / 2, d * 2, d * 2, 270, 360)
+        pop
+      when 2
+        arc(x - d / 2, y - d / 2, d * 2, d * 2, 0, 90) # このままでいい
+      when 3
+        push
+        translate(d, 0)
+        arc(x - d / 2, y - d / 2, d * 2, d * 2, 90, 180)
+        pop
+      when 4
+        push
+        translate(d, d)
+        arc(x - d / 2, y - d / 2, d * 2, d * 2, 180, 270)
+        pop
+      end
       y += d
     end
     x += d
