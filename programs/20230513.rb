58a365d519e8e2803f3bd5aae2dc19bab7d6884d
Miyuki Koshiba <m.koshiba@everyleaf.com>
2023-05-11 16:42:59 +0900

    中央にRubyKaigi2023ロゴを置く

--- programs/20230513.rb
+++ programs/20230513.rb
@@ -4,6 +4,7 @@ $colors = ["#ba083d", "#444444", "#a9a7ad", "#444444", "#a9a7ad"]
 def setup
   createCanvas($base * $split, $base * $split)
   angleMode(DEGREES)
+  rectMode(CENTER)
   noStroke
   noLoop
 end
@@ -42,4 +43,25 @@ def draw
     end
     x += d
   end
+  fill(255)
+  rect(width / 2, width / 2, $base * 3, $base * 3)
+  ruby_kaigi_logo
+end
+
+def ruby_kaigi_logo
+  fill($colors[0])
+  x = width / 2
+  y = width / 2
+  d = $base
+  ellipse(x, y, d, d)
+  ellipse(x - $base, y + $base, d, d)
+
+  fill($colors[1])
+  arc(x - $base / 2, y -  $base / 2, $base*2, $base*2, 90, 270)
+  arc(x + $base / 2, y +  $base / 2, $base*2, $base*2, 270, 90)
+
+  fill($colors[2])
+  arc(x - $base / 2 + $base, y - $base / 2 - $base, $base*2, $base*2, 90, 180)
+  arc(x + $base / 2, y - $base / 2, $base*2, $base*2, 270, 360)
+  arc(x - $base / 2, y + $base / 2 + $base, $base*2, $base*2, 270, 360)
 end
