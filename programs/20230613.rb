02effcf820d2b9006016780dcaf1b4b9d121d354
Miyuki Koshiba <ksbmyk@gmail.com>
2023-06-13 18:29:37 +0900

    パタパタさせた

--- programs/20230613.rb
+++ programs/20230613.rb
@@ -1,21 +1,33 @@
+# esa.io のﾄﾘ(\( ⁰⊖⁰)/)
 def setup
   createCanvas(600, 600)
   angleMode(DEGREES)
+  frameRate(10)
 end
 
 def draw
   background('#0a9b94')
   translate(300, 300)
-  # 白でマル
   noStroke
 
-  # 胴体
+  # 顔
   fill(255)
   ellipse(0, 0, 100, 108)
 
   # 羽
-  arc(-30, -20 , 110, 110,  90,  180)
-  arc(30, -20 , 110, 110,  0,  90)
+  if frameCount.to_i.even?
+    arc(-30, -20 , 110, 110,  90,  180)
+    arc(30, -20 , 110, 110,  0,  90)
+  else
+    push
+    rotate(10, 10)
+    arc(-30, -20 , 110, 110,  90,  180)
+    pop
+    push
+    rotate(-10, -10)
+    arc(30, -20 , 110, 110,  0,  90)
+    pop
+  end
 
   # 目
   fill('#666666')
