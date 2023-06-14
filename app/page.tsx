fb3a1329d3a0837d76fd955d4edeab81ff74f526
Miyuki Koshiba <ksbmyk@gmail.com>
2023-06-14 21:30:41 +0900

    一覧ページに画像を並べる

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,26 @@
 import Image from 'next/image'
+const images = [
+  '20230614.png',
+  '20230613.gif',
+  '20230612.png',
+  '20230514.png',
+  '20230513.gif',
+  '20230508.png',
+  '20230506.png',
+  '20230505.png',
+  '20230312.png',
+];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex">
-        <Image
-          className="w-1/3 mr-4"
-          src="/images/20230506.png"
-          alt=""
-          width={250}
-          height={37}
-        />
-        <Image
-            className="w-1/3 mr-4"
-            src="/images/20230506.png"
-            alt=""
-            width={250}
-            height={37}
-        />
-        <Image
-            className="w-1/3"
-            src="/images/20230506.png"
-            alt=""
-            width={250}
-            height={37}
-        />
-      </div>
+        <div className='grid-container'>
+          {images.map((image, index) => (
+              <div className='grid-item' key={index}>
+                <Image src={`/images/${image}`} alt="" width={250} height={250} />
+              </div>
+          ))}
+        </div>
     </main>
   )
 }
