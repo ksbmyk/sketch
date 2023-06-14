4446f8193427e7643e36e18fd0909307105124e7
Miyuki Koshiba <ksbmyk@gmail.com>
2023-06-14 18:52:10 +0900

    花を追加

--- programs/20230616.rb
+++ programs/20230616.rb
@@ -1,33 +1,40 @@
-$base = 80
-$split = 10
 $colors = %w(#9be9a8 #40c463 #30a14e #216e39)
-
 def setup
-  createCanvas($base * $split, $base * $split)
+  createCanvas(800, 800)
   angleMode(DEGREES)
-  background(255)
+  background('#a9ceec')
   noLoop
 end
 
 def draw
-  30.times do
+  100.times do
     draw_leaf(rand(1..10), rand(1..10))
   end
-  draw_lotus_flower
+
+  5.times do
+    draw_lotus_flower(rand(1..8), rand(1..8))
+  end
 end
 
 def draw_leaf(x, y)
   push
   rotate(rand(1..9)*20)
-  fill($colors[rand(0..3)])
+  c = color($colors[rand(0..3)])
+  c.setAlpha(200)
+  fill(c)
   noStroke
-  arc(x * 80 , - (y * 80), 160, 160, 0, 330)
+  arc(x*80 , - y*80, 160, 160, 0, 330)
   pop
 end
 
-def draw_lotus_flower
+def draw_lotus_flower(x, y)
   push
-  translate(200, 200)
+  translate(x*100, y*100)
+  rotate(rand(1..9)*20)
+  c = color('#fef3ff')
+  c.setAlpha(210)
+  fill(c)
+  noStroke
   n = 12
   n.times do
     ellipse(0, 20, 10, 40)
