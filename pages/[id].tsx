f5d36435d1c731ae6cf575959017f559b6c431d4
Miyuki Koshiba <ksbmyk@gmail.com>
2023-06-15 12:43:32 +0900

    描画ファイルを動的に指定

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -7,27 +7,28 @@ import './id.css'
 const DynamicPage = () => {
     const router = useRouter();
     const { id } = router.query;
-    const { query, isReady } = useRouter()
     const [fileContent, setFileContent] = useState('');
 
     useEffect(() => {
-        if (!isReady) return;
-        const filePath = `programs/${id}.rb`;
-        fetch(filePath)
-            .then((response) => response.text())
-            .then((data) => setFileContent(data))
-            .catch((error) => console.log(error));
-    }, [isReady, id]);
+        if (!router.isReady) return;
+            const filePath = `programs/${id}.rb`;
+            fetch(filePath)
+                .then((response) => response.text())
+                .then((data) => setFileContent(data))
+                .catch((error) => console.log(error));
+    }, [router.isReady, id]);
 
     return (
         <div>
-            <Head>
-                <script src="https://cdn.jsdelivr.net/npm/ruby-3_2-wasm-wasi@next/dist/browser.script.iife.js"></script>
-                <script src="https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.js"></script>
-                <script type="text/ruby" src="p5.rb"></script>
-                <script type="text/ruby" src="programs/20230614.rb"></script>
-                <script type="text/ruby">P5::init</script>
-            </Head>
+            {id && (
+                <Head>
+                    <script src="https://cdn.jsdelivr.net/npm/ruby-3_2-wasm-wasi@next/dist/browser.script.iife.js"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.js"></script>
+                    <script type="text/ruby" src="p5.rb"></script>
+                    <script type="text/ruby" src={`programs/${id}.rb`}></script>
+                    <script type="text/ruby">P5::init</script>
+                </Head>
+            )}
             <div className="grid-container">
                 <div className="grid-item">
                     <textarea className="bg-gray-200 p-3 text-sm h-full w-full" value={fileContent} readOnly />
