d7d8e3ce293a8088095057f02d0761152dcd19dd
Miyuki Koshiba <ksbmyk@gmail.com>
2023-06-15 21:05:10 +0900

    idに対応する静的ファイルを作成

--- next.config.js
+++ next.config.js
@@ -1,9 +1,34 @@
 /** @type {import('next').NextConfig} */
+const fs = require('fs');
+const path = require('path');
+
 const nextConfig = {
     output: 'export',
     assetPrefix: process.env.BASE_PATH || '',
     basePath: process.env.BASE_PATH || '',
     trailingSlash: false,
+    exportPathMap: async function () {
+        const paths = {
+            '/': { page: '/' }, // ルートページ
+        };
+
+        // programsディレクトリ内のファイルを取得
+        const directoryPath = path.join(__dirname, 'programs');
+        const files = fs.readdirSync(directoryPath);
+
+        // ファイル名からidを抽出してidsに追加
+        const ids = files.map((file) => {
+            const { name } = path.parse(file);
+            return name;
+        });
+
+        // 各idに対応するページを追加
+        ids.forEach((id) => {
+            paths[`/${id}`] = { page: '/[id]', query: { id } };
+        });
+        return paths;
+    },
+
 }
 
 module.exports = nextConfig
