57a59f62e55959d8953534d623fec88f7723e1d8
Miyuki Koshiba <ksbmyk@gmail.com>
2023-06-15 18:43:49 +0900

    デプロイのための設定を追加

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: process.env.GITHUB_ACTIONS && "/sketch",
+    trailingSlash: false,
+}
 
 module.exports = nextConfig
