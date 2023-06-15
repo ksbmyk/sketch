8aff946c807091344c49c9faee49c663a7fca0c3
Miyuki Koshiba <ksbmyk@gmail.com>
2023-06-15 21:20:13 +0900

    idを直接指定

--- next.config.js
+++ next.config.js
@@ -13,14 +13,16 @@ const nextConfig = {
         };
 
         // programsディレクトリ内のファイルを取得
-        const directoryPath = path.join(__dirname, 'programs');
-        const files = fs.readdirSync(directoryPath);
+        // const directoryPath = path.join(__dirname, 'programs');
+        // const files = fs.readdirSync(directoryPath);
 
         // ファイル名からidを抽出してidsに追加
-        const ids = files.map((file) => {
-            const { name } = path.parse(file);
-            return name;
-        });
+        // const ids = files.map((file) => {
+        //     const { name } = path.parse(file);
+        //     return name;
+        // });
+
+        const ids = ['20230614', '20230613'];
 
         // 各idに対応するページを追加
         ids.forEach((id) => {
