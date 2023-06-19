79d39293043cf7be4199e841b033681c280ae719
Miyuki Koshiba <ksbmyk@gmail.com>
2023-06-19 14:47:36 +0900

    add meta tag

--- components/DetailLayout.tsx
+++ components/DetailLayout.tsx
@@ -15,9 +15,17 @@ const DetailLayout = ({ children, title = 'sketch', id }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="icon" href="favicon.ico" />
+      <meta name='description' content='sketch stock with creative coding by chobishiba' key='desc' />
+      <meta name='twitter:card' content='summary' />
+      <meta property='og:title' content='top' />
+      <meta property='og:site_name' content='Sketch' />
+      <meta property='og:description' content={`${id} sketch by chobishiba`} />
+      <meta property='og:url' content={`https://ksbmyk.github.io/sketch/${id}`} />
+      <meta property='og:image' content={`https://ksbmyk.github.io/sketch/images/${id}.png`} />
+      <meta property='og:type' content='website' />
+      <link rel="icon" href="../favicon.ico" />
     </Head>
-    <header className="mx-4">
+    <header className="mx-4 mt-4">
       <nav>
         <Link href="/" className="text-customLink hover:text-customLinkHover">◀</Link>
           {!isMobile &&
@@ -29,7 +37,7 @@ const DetailLayout = ({ children, title = 'sketch', id }: Props) => (
       </nav>
     </header>
     {children}
-    <footer className="mx-4">
+    <footer className="mx-4 mb-4">
       <nav>
         <Link href="/" className="text-customLink hover:text-customLinkHover">◀</Link>
       </nav>
