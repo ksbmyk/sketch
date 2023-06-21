00faddf26926823868a769dd0547506adf4f2fc5
Miyuki Koshiba <ksbmyk@gmail.com>
2023-06-21 23:09:22 +0900

    rotate hydrangea

--- public/programs/20230616.rb
+++ public/programs/20230616.rb
@@ -1,45 +1,69 @@
-$colors = %w(#9be9a8 #40c463 #30a14e #216e39)
+# ref https://techty.hatenablog.com/entry/2019/05/29/194903
+
+$tile_count = 100
+$noise_scale = 0.02
+
 def setup
-  createCanvas(800, 800)
+  createCanvas(700, 700)
   angleMode(DEGREES)
-  background('#a9ceec')
+  background('#6EAAFF')
+  noStroke
   noLoop
 end
 
 def draw
-  100.times do
-    draw_leaf(rand(1..10), rand(1..10))
-  end
+  draw_tile
+  draw_ellipse
+  draw_hydrangea
 
-  5.times do
-    draw_lotus_flower(rand(1..8), rand(1..8))
+end
+
+def draw_tile
+  tile_size = width / $tile_count # tile1つの幅。10分割なら70
+  y_noise = 0
+  (0...$tile_count).each do |row|
+    x_noise = 0
+    (0...$tile_count).each do |col|
+      x = col * tile_size # 10分割なら座標は0,70,140,210…となる
+      y = row * tile_size
+      a = noise(x_noise, y_noise) * 255 # 指定した座標のパーリンノイズ値を取得(0〜1)、255を乗じて透明度（アルファ値）にする
+      noStroke
+      c = color(255, a) # color(gray, [alpha])
+      fill(c)
+      rect(x, y, tile_size, tile_size)
+      x_noise += $noise_scale
+    end
+    y_noise += $noise_scale
   end
 end
 
-def draw_leaf(x, y)
-  push
-  rotate(rand(1..9)*20)
-  c = color($colors[rand(0..3)])
-  c.setAlpha(200)
-  fill(c)
-  noStroke
-  arc(x*80 , - y*80, 160, 160, 0, 330)
-  pop
-end
-
-def draw_lotus_flower(x, y)
-  push
-  translate(x*100, y*100)
-  rotate(rand(1..9)*20)
-  c = color('#fef3ff')
-  c.setAlpha(210)
-  fill(c)
-  noStroke
-  n = 12
-  n.times do
-    ellipse(0, 20, 10, 40)
-    rotate(360/n)
+def draw_ellipse
+  stroke('#F0F8FF')
+  noFill
+  10.times do
+    random_number1 = rand(10..20)
+    random_number2 = rand(1..10)
+    ellipse(random_number1 * 10, random_number1 * 10,  random_number2 * 100, random_number2 * 100)
   end
-  pop
 end
 
+def draw_hydrangea
+  rounded_rect_size = 12
+  corner_radius = 3
+  rectMode(CENTER)
+  colors = %w(#9093E0 #9093E0 #9093E0)
+  (0..2).each do | i |
+    fill(colors[i])
+    noStroke
+    push
+    translate(width / 2, height / 2)
+    rotate(rand(1..3) * 15)
+    x = rand(-3..2) * 100
+    y = rand(-3..2) * 100
+    rect(x, y, rounded_rect_size, rounded_rect_size, corner_radius)
+    rect(x + rounded_rect_size, y, rounded_rect_size, rounded_rect_size, corner_radius)
+    rect(x, y + rounded_rect_size, rounded_rect_size, rounded_rect_size, corner_radius)
+    rect(x + rounded_rect_size, y + rounded_rect_size, rounded_rect_size, rounded_rect_size, corner_radius)
+    pop
+  end
+end
