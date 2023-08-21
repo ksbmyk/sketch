d4e92feb4ec219fe3312ca322b3e4c6d5f6099b4
Miyuki Koshiba <ksbmyk@gmail.com>
2023-08-21 19:11:42 +0900

    まずはベタ書きにする

--- public/programs/circle.rb
+++ public/programs/circle.rb
@@ -1,7 +1,5 @@
-# ruby会議大阪03用
-$base = 80
 def setup
-  createCanvas(600, 600)
+  createCanvas(500, 500)
   colorMode(RGB)
   angleMode(DEGREES)        #ラジアンではなく、360度で指定
   background(255, 255, 255)
@@ -12,17 +10,18 @@ def draw
   noLoop
   noStroke
 
-  # arc(x, y, w, h, start, stop)
+  # ellipse(x, y, w, h)
   # 赤丸
   fill('#ba083d')
-  arc($base, $base , $base * 2, $base * 2, 0, 360)
+  ellipse(50, 50, 100, 100)
 
+  # arc(x, y, w, h, start, stop)
   # グレー円弧
   fill('#a9a7ad')
-  arc($base * 3, $base , $base * 2, $base * 2,  90,  180)
-  arc($base * 4, $base , $base * 2, $base * 2,  180,  270)
+  arc(150, 50 , 100, 100,  90,  180)
+  arc(200, 50 , 100, 100,  180,  270)
 
   # グレー半円
   fill('#444444')
-  arc($base * 5,  $base , $base * 2, $base * 2,  0,  180)
+  arc(250, 50 , 100, 100,  0,  180)
 end
