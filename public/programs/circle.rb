29bc4bf603de4b3ff54337b2076f9e68cf3718ae
Miyuki Koshiba <ksbmyk@gmail.com>
2023-08-21 19:50:02 +0900

    コメント追加

--- public/programs/circle.rb
+++ public/programs/circle.rb
@@ -1,27 +1,22 @@
 def setup
   createCanvas(500, 500)
   colorMode(RGB)
-  angleMode(DEGREES)        #ラジアンではなく、360度で指定
+  angleMode(DEGREES)  #ラジアンではなく、360度で指定
   background(255, 255, 255)
 
 end
 
 def draw
-  noLoop
-  noStroke
+  noLoop  # draw()を1回だけ実行する
+  noStroke  # 輪郭線を描かない
 
-  # ellipse(x, y, w, h)
-  # 赤丸
-  fill('#ba083d')
-  ellipse(50, 50, 100, 100)
+  fill('#ba083d') # 円の赤
+  ellipse(50, 50, 100, 100) # ellipse(x, y, w, h)
 
-  # arc(x, y, w, h, start, stop)
-  # グレー円弧
-  fill('#a9a7ad')
-  arc(150, 50 , 100, 100,  90,  180)
+  fill('#a9a7ad') # 円弧のグレー
+  arc(150, 50 , 100, 100,  90,  180)   # arc(x, y, w, h, start, stop)
   arc(200, 50 , 100, 100,  180,  270)
 
-  # グレー半円
-  fill('#444444')
+  fill('#444444') # 半円の濃いグレー
   arc(250, 50 , 100, 100,  0,  180)
 end
