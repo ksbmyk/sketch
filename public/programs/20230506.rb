f5d3d0aa4a62f2f8ef1ce48ed951b96fb79e76da
Miyuki Koshiba <ksbmyk@gmail.com>
2023-08-26 09:46:20 +0900

    コメント削除

--- public/programs/20230506.rb
+++ public/programs/20230506.rb
@@ -1,21 +1,20 @@
 def setup
-  createCanvas(800, 800) # 画面のサイズ
-  colorMode(RGB)         # 色の指定をRGBモードにする
-  background(0, 30, 70)  # 背景色を指定(RGBで)
-  blendMode(OVERLAY)     # ブレンドモードを指定(OVERLAY)
+  createCanvas(800, 800)
+  colorMode(RGB)
+  background(0, 30, 70)
+  blendMode(OVERLAY)
 end
 
 def draw
-  noLoop                        # draw()を1回だけ実行する
-  1500.times do | i |           # 1500回繰り返す
-    if i.even?                  # iが偶数の時
-      noStroke                  # 輪郭線を描かない
-      fill(170, 200, 255)       # 塗りの色(RGBで)
+  noLoop
+  1500.times do | i |
+    if i.even?
+      noStroke
+      fill(170, 200, 255)
     else 
-      stroke(170, 200, 255)     # 輪郭線の色を指定
-      fill(255, 255, 255, 100)  # 塗りの色(RGBAで)
+      stroke(170, 200, 255)
+      fill(255, 255, 255, 100)
     end
-    # 円を描く。位置は0-900の間でランダムに決める。大きさは30
     ellipse(random(0, 900), random(0, 900), 30)
   end
 end
