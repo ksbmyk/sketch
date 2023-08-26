5d6747307b74c09fbaf00cae8c1acaffff38d8ba
Miyuki Koshiba <ksbmyk@gmail.com>
2023-08-26 09:47:58 +0900

    色をランダムに

--- public/programs/lattice.rb
+++ public/programs/lattice.rb
@@ -1,6 +1,6 @@
 $base = 80
 $split = 9
-$colors = %w(#ba083d #444444 #a9a7ad)
+$colors = ["#ba083d", "#444444", "#a9a7ad"]
 def setup
   createCanvas($base * $split, $base * $split)
   angleMode(DEGREES)
@@ -8,34 +8,17 @@ def setup
 end
 
 def draw
-  noStroke
   noLoop
+  noStroke
   x = 0
   while x < width do
     y = 0
     while y < width do
-      fill("#444444")
-      #ellipse(x + $base/2, y + $base/2, $base , $base )
-
-      #arc(x  , y , $base * 2, $base * 2, 0, 90)
-
-      push
-      translate($base, 0)
-      #arc(x  , y , $base * 2, $base * 2, 90, 180)
-      pop
-
-      push
-      translate($base, $base)
-      #arc(x  , y , $base * 2, $base * 2, 180, 270)
-      pop
-
-      push
-      translate(0, $base)
-      arc(x  , y , $base * 2, $base * 2, 270, 360)
-      pop
-
+      fill($colors[rand(0..2)])
+      arc(x, y, $base * 2, $base * 2, 0, 90)
       y += $base
     end
     x += $base
   end
 end
+
