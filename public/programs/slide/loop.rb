ffd77ebf2f06e3b1feb048713042cce8a2531517
Miyuki Koshiba <ksbmyk@gmail.com>
2023-08-30 19:01:33 +0900

    loop用サンプル調整

--- public/programs/slide/loop.rb
+++ public/programs/slide/loop.rb
@@ -1,12 +1,10 @@
 def setup
-  createCanvas(400, 400) # 画面のサイズ
-  background('#001e46')  # 背景色を指定
-  blendMode(OVERLAY)     # ブレンドモードを指定(OVERLAY)
+  createCanvas(400,400)
+  background(255)
 end
 
 def draw
-    noStroke                  # 輪郭線を描かない
-    fill('#aac8ff')
-    # 円を描く。位置は0-900の間でランダムに決める。大きさは30
-    ellipse(random(0, 400), random(0, 400), 30)
+  noStroke
+  fill('#aac8ff')
+  ellipse(rand(0..400), rand(0..400), 30, 30)
 end
