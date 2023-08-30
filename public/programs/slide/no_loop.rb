cd4c9a7e1ddd7b00426dbe5f44dc72257b422883
Miyuki Koshiba <ksbmyk@gmail.com>
2023-08-30 19:09:32 +0900

    noLoopサンプルを整えた

--- public/programs/slide/no_loop.rb
+++ public/programs/slide/no_loop.rb
@@ -1,15 +1,13 @@
 def setup
-  createCanvas(400, 400) # 画面のサイズ
-  colorMode(RGB)         # 色の指定をRGBモードにする
-  background(0, 30, 70)  # 背景色を指定(RGBで)
-  blendMode(OVERLAY)     # ブレンドモードを指定(OVERLAY)
+  createCanvas(400,400)
+  background(255)
 end
 
 def draw
-  noLoop                        # draw()を1回だけ実行する
-  1500.times do | i |           # 1500回繰り返す
-    noStroke                  # 輪郭線を描かない
-    fill(170, 200, 255)       # 塗りの色(RGBで)
-    ellipse(random(0, 400), random(0, 400), 30)
+  noLoop
+  100.times do
+    noStroke
+    fill('#aac8ff')
+    ellipse(rand(0..400), rand(0..400), 30, 30)
   end
 end
