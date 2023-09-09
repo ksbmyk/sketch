75b26039c0a973aec28053161048042125da60b8
Miyuki Koshiba <ksbmyk@gmail.com>
2023-09-09 17:43:29 +0900

    Add grid sample for Osaka Ruby Kaigi 03

---
+++ public/programs/20230907.rb
@@ -0,0 +1,27 @@
+$side = 80
+$split = 9
+def setup
+  createCanvas($side * $split, $side * $split)
+  background(0)
+  blendMode(ADD)
+end
+
+def draw
+  noLoop
+  noStroke
+  x = 0
+  while x <= width do
+    y = 0
+    while y <= width do
+
+      r = random(255)
+      g = random(255)
+      b = random(255)
+      a = random(100, 200)
+      fill(r, g, b, a)
+      ellipse(x, y, $side * 2, $side * 2)
+      y += $side
+    end
+    x += $side
+  end
+end
