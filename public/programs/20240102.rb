7b3083760203bfa8cf4fcc0ea414bc964f423aa8
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-02 15:54:17 +0900

    change color

--- public/programs/20240102.rb
+++ public/programs/20240102.rb
@@ -16,15 +16,28 @@ def draw
   while x <= width do
     y = 0
     while y <= height do
-      r = random(255)
+      r = random(50)
       g = random(255)
       b = random(255)
       a = random(100, 200)
       fill(r, g, b, a)
-      ellipse(x, y, $side, $side * 2)
+      diamond_shape($side/2, $side, x, y)
       y += $side
     end
-    x += $side
+    x += $side / 2
   end
 end
 
+def diamond_shape(w, h, x, y)
+  push()
+  translate(x, y)
+
+  beginShape()
+  vertex(0, -h)
+  vertex(w, 0)
+  vertex(0, h)
+  vertex(-w, 0)
+  endShape()
+  
+  pop()
+end
