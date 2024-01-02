2eb6c5a7b42b5d3b298f2a734f13adbee5eae78d
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-02 13:35:20 +0900

    add base

---
+++ public/programs/20240105.rb
@@ -0,0 +1,25 @@
+# GENUARY 2024 jan5 In the style of Vera Molnár (1924-2023).
+# https://genuary.art/prompts
+
+$side = 80
+$split = 9
+
+def setup
+  createCanvas($side * $split, $side * $split)
+  background(255)
+end
+
+def draw
+  noLoop
+  noStroke
+  x = 0
+  while x < width - $side do
+    y = 0
+    while y < width - $side do
+      fill("#54AEFF")
+      rect(x, y, $side - 10)
+      y += $side
+    end
+    x += $side
+  end
+end
