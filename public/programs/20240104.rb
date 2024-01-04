9ad38fdd6bcac3aefbb0675e8ce10e9e4980dd70
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-04 22:17:02 +0900

    change color and placement

--- public/programs/20240104.rb
+++ public/programs/20240104.rb
@@ -2,22 +2,70 @@
 # https://genuary.art/prompts
 
 $base = 10
-$split = 9
-$colors = %w(#B6E3FF #54AEFF #0969DA #0A3069)
+$split = 20
+$colors = %w(#ffee4a #ffc501 #fe9600 #77477e)
 def setup
-  createCanvas($base * $split, $base * $split)
+  createCanvas($base * $split * 3, $base * $split * 3)
   angleMode(DEGREES)
-  background(0)
-  noLoop
+  background(255)
+  frameRate(1)
 end
 
 def draw
-  background(255)
-  #d = width / $split
-  x = $base
-  while x < width do
-    y = $base
-    while y < height do
+  if frameCount.to_i.odd?
+    background(255)
+
+    pixel_box
+    
+    push
+    translate($base * $split, $base * $split)
+    pixel_box
+    pop
+    
+    push
+    translate($base * $split * 2, 0)
+    pixel_box
+    pop
+
+    push
+    translate(0, $base * $split * 2)
+    pixel_box
+    pop
+    
+    push
+    translate($base * $split * 2, $base * $split * 2)
+    pixel_box
+    pop
+    
+  else
+    background(255)
+    push
+    translate($base * $split, 0)
+    pixel_box
+    pop
+
+    push
+    translate(0, $base * $split)
+    pixel_box
+    pop
+
+    push
+    translate($base * $split * 2, $base * $split)
+    pixel_box
+    pop
+    
+    push
+    translate($base * $split, $base * $split * 2)
+    pixel_box
+    pop
+  end
+end
+
+def pixel_box
+  x = 0
+  while x < $base * $split do
+    y = 0
+    while y < $base * $split do
       noStroke
       c = rand(0..3)
       fill($colors[c])
