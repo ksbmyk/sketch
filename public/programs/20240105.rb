650f39899047fb432c11d7d949d0b5ec414108eb
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-05 22:56:07 +0900

    change color

--- public/programs/20240105.rb
+++ public/programs/20240105.rb
@@ -7,22 +7,27 @@ $split = 12
 def setup
   createCanvas($side * $split + 20 , $side * $split + 20)
   colorMode(HSB, 360, 100, 100, 100);
-  background(255)
+  background(0,0,100)
 end
 
 def draw
   translate(10, 10)
-  noise_scale = 0.03
   noLoop
   noStroke
+  count = 0
   x = 0
   while x < width - $side do
     y = 0
     while y < height - $side do
-      fill(208, 67, 100, 70)
+      if count.odd?
+        fill(270, 55, 100, 70)
+      else
+        fill(208, 67, 100, 70)
+      end
       rect(x + rand(-4..5), y + rand(-5..10), $side - 10, $side - 3)
       y += $side
     end
     x += $side
+    count += 1
   end
 end
