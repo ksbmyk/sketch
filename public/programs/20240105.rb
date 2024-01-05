34d39c9e738b6e6f587a6cc96c02970675c7a9d0
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-05 22:28:16 +0900

    add fluctuation

--- public/programs/20240105.rb
+++ public/programs/20240105.rb
@@ -1,24 +1,26 @@
 # GENUARY 2024 jan5 In the style of Vera Molnár (1924-2023).
 # https://genuary.art/prompts
 
-$side = 80
-$split = 9
+$side = 60
+$split = 12
 
 def setup
-  createCanvas($side * $split, $side * $split)
+  createCanvas($side * $split + 20 , $side * $split + 20)
+  colorMode(HSB, 360, 100, 100, 100);
   background(255)
 end
 
 def draw
+  translate(10, 10)
+  noise_scale = 0.03
   noLoop
   noStroke
   x = 0
   while x < width - $side do
     y = 0
-    while y < width - $side do
-      fill("#54AEFF")
-      #noise(x_noise, y_noise) * 255
-      rect(noise(x), noise(y), $side - 10)
+    while y < height - $side do
+      fill(208, 67, 100, 70)
+      rect(x + rand(-4..5), y + rand(-5..10), $side - 10, $side - 3)
       y += $side
     end
     x += $side
