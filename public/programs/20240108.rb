e3a5e08ef9d09a1edc03e8272164d65befaedbf9
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-06 23:50:13 +0900

    add grow

--- public/programs/20240108.rb
+++ public/programs/20240108.rb
@@ -4,40 +4,52 @@
 # https://www.isc.meiji.ac.jp/~random/lecture/2017-comp2/miyake.html
 
 def setup
-    @x = 0.01
-    @y = 0
-    @z = 0
-    @dt = 0.01
-    @sigma = 10
-    @rho = 28
-    @beta = 8/3
-    
-    createCanvas(800, 600, WEBGL) # 三次元描画
-    background(0)
+  @x = 0.01
+  @y = 0
+  @z = 0
+  @dt = 0.01
+  @sigma = 10
+  @rho = 28
+  @beta = 8/3
 
-  end
+  createCanvas(800, 600, WEBGL) # 三次元描画
+  background(0)
+  blendMode(SCREEN)
+end
   
-  def draw
-    translate(0, 0, -80)
-    
-    # ローレンツ方程式
-    dx = (@sigma * (@y - @x)) * @dt
-    dy = (@x * (@rho - @z) - @y) * @dt
-    dz = (@x * @y - @beta * @z) * @dt
-    
-    @x += dx
-    @y += dy
-    @z += dz
-    
-    px = map(@x, -20, 20, -width / 2, width / 2)
-    py = map(@y, -20, 20, -height / 2, height / 2)
-    pz = map(@z, 0, 40, -200, 200)
-  
-    from = color(255, 0, 0) # 色の開始点
-    to = color(0, 0, 255) # 色の終了点
-    interColor = lerpColor(from, to, map(pz, -200, 200, 0, 1))
+def draw
+  translate(0, 0, -80)
 
-    fill(interColor)
+  # ローレンツ方程式
+  dx = (@sigma * (@y - @x)) * @dt
+  dy = (@x * (@rho - @z) - @y) * @dt
+  dz = (@x * @y - @beta * @z) * @dt
+
+  @x += dx
+  @y += dy
+  @z += dz
+
+  px = map(@x, -20, 20, -width / 2, width / 2)
+  py = map(@y, -20, 20, -height / 2, height / 2)
+  pz = map(@z, 0, 40, -200, 200)
+
+  # 光
+  1..20.times do | i |
+    strokeWeight(i)
+    stroke(5, 10, 35 - i, 100)
     ellipse(px, py, 3, 3)
   end
+
+  from = color(255, 0, 0) # 色の開始点
+  to = color(0, 0, 255) # 色の終了点
+  interColor = lerpColor(from, to, map(pz, -200, 200, 0, 1))
+
+  stroke(interColor)
+  strokeWeight(5)
+  point(px, py, pz)
+
+  if(frameCount > 5000)
+    noLoop()
+  end
+end
  
