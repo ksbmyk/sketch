4bc212b384cf2ddf02bb82418f185a71767999a4
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-07 12:46:48 +0900

    add base

---
+++ public/programs/20240109.rb
@@ -0,0 +1,38 @@
+attr_reader :bubbles
+
+def setup
+  createCanvas(400, 400)
+  textSize(30)
+  @bubbles = Array.new(10) { Bubble.new(rand(width), rand(-200.0..-100.0)) }
+end
+
+def draw
+  background(255)
+  @bubbles.each do |bubble|
+    bubble.fall
+    bubble.display
+  end
+end
+
+class Bubble 
+  attr_accessor :x, :y, :speed, :ascii_character
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @speed = rand(1.0..5.0)
+    @ascii_character = "*"
+  end
+
+  def fall
+    @y += speed
+    if (@y > height + 20) 
+      @y = rand(-200.0..-100.0)
+    end
+  end
+
+  def display
+    fill(0)
+    text(ascii_character, x, y)
+  end
+end
