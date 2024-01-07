0325fbb63478124b8b1b183995541f45e13bd261
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-07 13:43:48 +0900

    "*" -> ";"

--- public/programs/20240109.rb
+++ public/programs/20240109.rb
@@ -1,13 +1,17 @@
+# GENUARY 2024 jan6 "ASCII."
+# https://genuary.art/prompts
+# rbCanvasなら動く https://rbcanvas.net/p5/0.4.0/editor/rbcanvasp5_editor.html
+
 attr_reader :bubbles
 
 def setup
-  createCanvas(400, 400)
+  createCanvas(600, 600)
   textSize(30)
-  @bubbles = Array.new(10) { Bubble.new(rand(width), rand(-200.0..-100.0)) }
+  @bubbles = Array.new(30) { Bubble.new(rand(width), rand(-200.0..-100.0)) }
 end
 
 def draw
-  background(255)
+  background("blue")
   @bubbles.each do |bubble|
     bubble.fall
     bubble.display
@@ -20,8 +24,8 @@ class Bubble
   def initialize(x, y)
     @x = x
     @y = y
-    @speed = rand(1.0..5.0)
-    @ascii_character = "*"
+    @speed = rand(3.0..8.0)
+    @ascii_character = ";"
   end
 
   def fall
@@ -32,7 +36,7 @@ class Bubble
   end
 
   def display
-    fill(0)
+    fill(255)
     text(ascii_character, x, y)
   end
 end
