4e5f5ce1dfa6c264d19e754d0924d6829b05b794
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-08 13:22:01 +0900

    renam

--- public/programs/20240112.rb
+++ public/programs/20240112.rb
@@ -25,25 +25,25 @@ def draw
     circle[:x] += circle[:speed_x]
     circle[:y] += circle[:speed_y]
 
-    (0...@circles.length).each do |j| # 他の円
+    (0...@circles.length).each do |j| # 他の円すべて
       if (i != j)
         other = @circles[j] # 他の円
 
         # 円同士の距離を計算
         dx = circle[:x] - other[:x]
         dy = circle[:y] - other[:y]
-        distance = sqrt(dx * dx + dy * dy)
-        min_dist = circle[:diameter] / 2 + other[:diameter] / 2
+        distance = sqrt(dx * dx + dy * dy) # 2つの円の中心間の距離
+        min_dist = circle[:diameter] / 2 + other[:diameter] / 2 # 円同士が衝突するための最小の距離
 
         # 円同士がぶつかったとき
         if (distance < min_dist)
           # atan2 直角三角形の、斜辺でない2辺の長さから、x軸との角度をとる
           angle = atan2(dy, dx) # ぶつかった方向を計算
 
-          targetX = other[:x] + cos(angle) * min_dist # 衝突時の目標x座標を計算
-          targetY = other[:y] + sin(angle) * min_dist # 衝突時の目標y座標を計算
-          ax = (targetX - circle[:x]) * 0.1 # 加速度（x方向）を計算
-          ay = (targetY - circle[:y]) * 0.1 # 加速度（y方向）を計算
+          target_x = other[:x] + cos(angle) * min_dist # 衝突時の目標x座標を計算
+          target_y = other[:y] + sin(angle) * min_dist # 衝突時の目標y座標を計算
+          ax = (target_x - circle[:x]) * 0.1 # 加速度（x方向）を計算
+          ay = (target_y - circle[:y]) * 0.1 # 加速度（y方向）を計算
           circle[:speed_x] += ax # x方向の速度を更新
           circle[:speed_y] += ay # y方向の速度を更新
         end
@@ -59,9 +59,10 @@ def draw
       circle[:speed_y] *= -1 # 速度を反転
     end
 
+    # 速度上限を設けて超えないようにする
     max_speed = 2
-    # 速度の大きさを計算
-    # sqrt 平方根
+    # 円の現在の速度の大きさを計算
+    # sqrt 平方根 速度の大きさ（ベクトルの長さ）をとる
     speed_magnitude = sqrt(circle[:speed_x] ** 2 + circle[:speed_y] ** 2)
     if (speed_magnitude > max_speed)
       ratio = max_speed / speed_magnitude
