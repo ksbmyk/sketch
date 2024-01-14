c117b0897d4765efa4cdd27e24315e726724d34d
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-14 17:52:46 +0900

    use 2 color

--- public/programs/20240116.rb
+++ public/programs/20240116.rb
@@ -2,7 +2,6 @@ def setup
     createCanvas(720, 720)
     background(255)
   	colorMode(HSB, 360, 100, 100, 100)
-    #blendMode(SOFT_LIGHT)
     noLoop
   end
   
@@ -10,7 +9,11 @@ def setup
 	size = 10
     noStroke
     10000.times do |i|
-      fill(60, 80, 80, 30)
+      if i < 5000
+        fill(200+rand(0..20), 80+rand(0..20), 80, 30)
+      else
+        fill(100+rand(0..20), 80+rand(0..20), 80, 30)
+      end
       rect(rand(0..width - size), rand(0..width - size), size)
     end
   end
