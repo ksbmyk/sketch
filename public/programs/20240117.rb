581d5412a230e1e20d8b8e30a629f69cf4f4fd13
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-14 13:44:38 +0900

    add color

--- public/programs/20240117.rb
+++ public/programs/20240117.rb
@@ -1,16 +1,23 @@
 def setup
     createCanvas(400, 400)
-    background(255)
+    background("blue")
     noLoop()
   end
   
   def draw
     translate(width / 2, height / 2)
+    
+    stroke(255)
+    strokeWeight(10)
+    noFill()
+    
+    drawArabesque(0, 0, 100, 5, 5)
+    drawArabesque(0+90, 0, 100, 5, 5)
+    
     stroke(0)
     strokeWeight(2)
     noFill()
     
-  
     drawArabesque(0, 0, 100, 5, 5)
     drawArabesque(0+90, 0, 100, 5, 5)
     drawArabesque(0+90*2, 0, 100, 5, 5)
@@ -18,6 +25,8 @@ def setup
     drawArabesque(0+45, 0+75, 100, 5, 5)
     drawArabesque(0+45+90, 0+75, 100, 5, 5)
     
+    # fill("yellow")
+    # circle(0, 0, 10)
     
   end
   
@@ -26,15 +35,21 @@ def setup
     
     beginShape()
     sides.times do |i|
+  #   for (let i = 0; i < sides; i++) {
       angle = map(i, 0, sides, 0, TWO_PI)
       newX = x + cos(angle) * radius
       newY = y + sin(angle) * radius
       vertex(newX, newY)
       
-      nextRadius = radius * 0.3 # Adjust this factor to control the size of the next iteration
+      nextRadius = radius * 0.3; # Adjust this factor to control the size of the next iteration
       nextX = x + cos(angle) * nextRadius
       nextY = y + sin(angle) * nextRadius
       drawArabesque(nextX, nextY, nextRadius, sides, depth - 1)
+      
+      # fill("yellow")
+      # circle(x, y, 5)
+      # noFill
     end
     endShape(CLOSE)
   end
+  
