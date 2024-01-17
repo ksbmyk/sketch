a5f62bc66a4a5516d1207c0b9b7f9cdafa4a2a2a
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-17 22:30:11 +0900

    change octagon

--- public/programs/20240117.rb
+++ public/programs/20240117.rb
@@ -2,55 +2,55 @@
 # https://genuary.art/prompts
 
 def setup
-    createCanvas(600, 600)
-    background("#335fa6")
-    noLoop
-  end
+  createCanvas(600, 600)
+  background("#335fa6")
+  noLoop
+end
   
-  def draw
-    stroke(255)
-    strokeWeight(10)
-    noFill
-    
-    arabesque_pattern
-    
-    stroke("#7eaab7")
-    strokeWeight(2)
-    noFill
-    
-    arabesque_pattern
+def draw
+  stroke(255)
+  strokeWeight(10)
+  noFill
+  
+  arabesque_pattern
+  
+  stroke("#7eaab7")
+  strokeWeight(2)
+  noFill
+  
+  arabesque_pattern
 
-    noStroke
-    fill("#c7a964")
-    circle_pattern
-    
-  end
+  noStroke
+  fill("#c7a964")
+  circle_pattern
+  
+end
 
 def arabesque_pattern
   (0..8).step do |i|
-    arabesque(90*i   , 0   , 100, 5, 5)
-    arabesque(45+90*i, 75  , 100, 5, 5)
-    arabesque(90*i   , 75*2, 100, 5, 5)
-    arabesque(45+90*i, 75*3, 100, 5, 5)
-    arabesque(90*i   , 75*4, 100, 5, 5)
-    arabesque(45+90*i, 75*5, 100, 5, 5)
-    arabesque(90*i   , 75*6, 100, 5, 5)
-    arabesque(45+90*i, 75*7, 100, 5, 5)
-    arabesque(90*i   , 75*8, 100, 5, 5)
+    arabesque(90*i, 80*0, 100, 8, 5)
+    arabesque(90*i, 80*1, 100, 8, 5)
+    arabesque(90*i, 80*2, 100, 8, 5)
+    arabesque(90*i, 80*3, 100, 8, 5)
+    arabesque(90*i, 80*4, 100, 8, 5)
+    arabesque(90*i, 80*5, 100, 8, 5)
+    arabesque(90*i, 80*6, 100, 8, 5)
+    arabesque(90*i, 80*7, 100, 8, 5)
+    arabesque(90*i, 80*8, 100, 8, 5)
   end
 end
 
 def circle_pattern
   (0..8).step do |i|
-    circle(90*i   , 0, 10)
-    circle(45+90*i, 75, 10)
-    circle(90*i, 75*2, 10)
-    circle(45+90*i, 75*3, 10)
-    circle(90*i, 75*4, 10)
-    circle(45+90*i, 75*5, 10)
-    circle(90*i, 75*6, 10)
-    circle(45+90*i, 75*7, 10)
-    circle(90*i, 75*8, 10)
+    circle(90*i, 0, 10)
+    circle(90*i, 80, 10)
+    circle(90*i, 80*2, 10)
+    circle(90*i, 80*3, 10)
+    circle(90*i, 80*4, 10)
+    circle(90*i, 80*5, 10)
+    circle(90*i, 80*6, 10)
+    circle(90*i, 80*7, 10)
+    circle(90*i, 80*8, 10)
   end
 end
   
@@ -68,7 +68,7 @@ def arabesque(x, y, radius, sides, depth)
     next_x = x + cos(angle) * next_radius
     next_y = y + sin(angle) * next_radius
     # p5.rbだと重いのでrbCanvasで動かすなら外してよい
-    # arabesque(next_x, next_y, next_radius, sides, depth - 1)
+    arabesque(next_x, next_y, next_radius, sides, depth - 1)
   end
   endShape(CLOSE)
 end
