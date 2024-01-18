6851e54faf30b4a3ac1928d3a295899590afb38a
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-18 21:05:09 +0900

    change count

--- public/programs/20240117.rb
+++ public/programs/20240117.rb
@@ -25,15 +25,22 @@ def draw
 end
 
 def arabesque_pattern
-  (0..8).step do |i|
-    (0..8).each { |j| arabesque(90 * i, 80 * j, 100, 8, 5) }
-  end
+  # 再帰がp5.rbだと重いので表示回数を減らす(rbCanvasでならもとの個数でも可能)
+  # (0..8).step do |i|
+  #   (0..8).each { |j| arabesque(90 * i, 80 * j, 100, 8, 5) }
+  # end
+  i = 3
+  j = 3
+  arabesque(90 * i, 80 * j, 100, 8, 5)
 end
 
 def circle_pattern
-  (0..8).step do |i|
-    (0..8).each { |j| circle(90 * i, 80 * j, 10) }
-  end
+  # (0..8).step do |i|
+  #   (0..8).each { |j| circle(90 * i, 80 * j, 10) }
+  # end
+  i = 3
+  j = 3
+  circle(90 * i, 80 * j, 10)
 end
   
 def arabesque(x, y, radius, sides, depth)
@@ -49,8 +56,7 @@ def arabesque(x, y, radius, sides, depth)
     next_radius = radius * 0.3 # 反復サイズ
     next_x = x + cos(angle) * next_radius
     next_y = y + sin(angle) * next_radius
-    # p5.rbだと重いのでrbCanvasで動かすなら外してよい
-    #arabesque(next_x, next_y, next_radius, sides, depth - 1)
+    arabesque(next_x, next_y, next_radius, sides, depth - 1)
   end
   endShape(CLOSE)
 end
