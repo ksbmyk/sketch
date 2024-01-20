a7e9a0b91c38eae4b58ac86f470faa12515533b0
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-20 18:17:54 +0900

    change shadow

--- public/programs/20240120.rb
+++ public/programs/20240120.rb
@@ -15,12 +15,12 @@ def setup()
   @textGraphic.text(@text, width / 2, height / 2)
 end
 
-def draw() 
-  noLoop
-  draw_background
-
-  drawReflection(@textGraphic, 0.5, 25)
-  main_text
+def draw
+    noLoop
+    y_offset = 25
+    draw_background(y_offset)
+    drawReflection(@textGraphic, 0.5, y_offset)
+    main_text
 end
 
 def main_text 
@@ -33,23 +33,22 @@ def main_text
   text(@text, width / 2, height / 2)
 end
 
-def draw_background()
-(0..height).each do |y|
+def draw_background(y_offset)
+  (y_offset..height).each do |y|
     gradient = map(abs(y - height / 2), 0, height / 2, 255, 0)
     stroke(lerpColor(color(0, 120, 255), color(0, 0, 255), gradient.to_f / 255))
     line(0, y, width, y)
-end
+  end
 end
 
 def drawReflection(graphic, alpha, y_offset) 
   cg = createGraphics(width, height)
   cg.background(255)
-  cg.translate(0, height + y_offset)
-  cg.scale(1, -1) # y軸を反転
   cg.image(graphic, 0, 0, width, height)
     
-  push()
+  push
   tint(255, alpha * 255) 
   cg.filter(BLUR, 5) # ぼかし
-  image(cg, 0, 0, width, height)
+  image(cg, 0, y_offset, width, height)
+  pop
 end
