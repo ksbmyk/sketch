01d8560a7517c54947ff8a9645b12f189ad57a28
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-22 23:09:28 +0900

    add point line plane

--- public/programs/20240122.rb
+++ public/programs/20240122.rb
@@ -3,22 +3,38 @@
 
 def setup
   createCanvas(720, 720)
-  background("#f2e9c0")
+  background("#b09d75")
   noLoop
 end
 
 def draw
-  noFill
-  circle(200, 200, 200)
-  rect(200, 250, width - 200*2, 400)
+  noStroke
 
+  fill("#696343")
+  circle(200, 200, 200)
+  fill("#2b5563")
+  rect(200, 250, width - 200 * 2, 400)
+
+  fill("#624a40")
+  circle(rand(50..width / 2), rand(50..height / 2), 50)
+  fill(0)
+  circle(rand(50..width / 2), rand(50..height / 2), 30)
+  fill("#3e221e")
+  rect(rand(100..width - 100), rand(100..height - 100), rand(2..5)*100, rand(1..3) * 50)
+
+  fill("#843f20")
+
+  triangle(rand(width / 2..width), rand(0..height / 2), rand(width / 2..width), rand(0..height / 2), rand(width / 2..width), rand(0..height / 2) )
+  triangle(rand(0..width/2),rand(height/2..height), rand(0..width/2),rand(height/2..height), rand(0..width/2), rand(height/2..height) )
+  
+  fill(255,255,255,80)
+  circle(rand(50..width / 2), 500, rand(200..300))
+
+  stroke(1)
   w = 500
   5.times do |i|
-    #strokeWeight(3)
-    line(w+i*30, height/2+100, w+i*30, height) # 縦
-    line(width/2+100, w+i*30, width, w+i*30) # 縦
+    line(w + i * 30, height / 2 + 100, w + i * 30, height)
+    line(width / 2 + 100, w + i * 30, width, w + i * 30)
   end
-  #circle(rand(50..width/2), rand(50..height/2), rand(1..5)*50)
-  #rect(rand(100..width-100), rand(100..height-100), rand(2..5)*100, rand(1..3)*50, )
 end
 
