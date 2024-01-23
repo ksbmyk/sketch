23dab1ac3f7d716ca23ed374596b6f2bffd34526
Miyuki Koshiba <ksbmyk@gmail.com>
2024-01-23 22:15:56 +0900

    add base

---
+++ public/programs/20240124.rb
@@ -0,0 +1,54 @@
+# GENUARY 2024 jan24 "Impossible objects (undecided geometry)."
+# https://genuary.art/prompts
+
+def setup
+  createCanvas(500, 500)
+  @c1 = color(random(255), random(255), random(255))
+  @c2 = color(random(255), random(255), random(255))
+  @c3 = color(random(255), random(255), random(255))
+  noLoop
+end
+
+def draw
+  background(255)
+  noStroke
+  translate(width / 2, height * 3 / 5)
+  fill(@c1)
+  draw_object
+  rotate(TWO_PI / 3)
+  fill(@c2)
+  draw_object
+  rotate(TWO_PI / 3)
+  fill(@c3)
+  draw_object()
+end
+
+def draw_object
+  e = 110 
+  w = 60
+  sin30 = sin(PI / 6)
+  cos30 = cos(PI / 6)
+  sin60 = sin(PI / 3)
+  cos60 = cos(PI / 3)
+
+  beginShape()
+  x1 = -e * cos60
+  y1 = (e * cos60) / sqrt(3)
+  vertex(x1, y1)
+  x2 = x1 - w
+  y2 = y1
+  vertex(x2, y2)
+  x3 = x2 + (e + 3.0 * w) * cos60
+  y3 = y2 - (e + 3.0 * w) * sin60
+  vertex(x3, y3)
+  x4 = x3 + (e + 4.0 * w) * sin30
+  y4 = y3 + (e + 4.0 * w) * cos30
+  vertex(x4, y4)
+  x5 = x4 - w * cos60
+  y5 = y4 + w * sin60
+  vertex(x5, y5)
+  x6 = x5 - (e + 3 * w) * cos60
+  y6 = y5 - (e + 3 * w) * sin60
+  vertex(x6, y6)
+  endShape(CLOSE)
+end
