82172ba7493b83ec7582c529397b2b498eae65d3
Miyuki Koshiba <ksbmyk@gmail.com>
2024-02-27 22:49:49 +0900

    use proc

--- public/programs/20240302.rb
+++ public/programs/20240302.rb
@@ -8,18 +8,20 @@ def setup
 end
 
 def draw
-  stroke(0)
+
+  bridge = Proc.new do |color, size|
+    stroke(0)
+    fill(color)
+    arc(width/2, height/2, size, size, 180, 360)
+  end
+
   colors = ['#eeb3b2', '#d5e8c5', '#bca7c7']
   radius = 210
+
   5.times do |i|
     color = colors[i % colors.length]
-    bridge(color, radius)
+    bridge.call(color, radius)
     radius = radius - (- 5*i + 30)
   end
-  bridge('#ffffff', 110)  
-end
-
-def bridge(color, size)
-  fill(color)
-  arc(width/2, height/2, size, size, 180, 360)
+  bridge.call('#ffffff', 110) 
 end
