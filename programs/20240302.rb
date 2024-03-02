5b08564985ff30f3d81de0d8fb0e509ba81be639
ksbmyk <ksbmyk@users.noreply.github.com>
2024-03-02 13:59:54 +0000

    deploy: 8d7854bf81e30649560475cce3e5b7c647940b56

---
+++ programs/20240302.rb
@@ -0,0 +1,29 @@
+## Rails Girls Tokyo 16th https://kaigionrails.org/2023 のロゴ design by moegi29
+
+def setup
+  createCanvas(500, 500)
+  angleMode(DEGREES)
+  background(255)
+  noLoop
+end
+
+def draw
+  bridge = Proc.new do |options|
+    stroke(0)
+    fill(options[:color])
+    arc(width/2, height/2+30, options[:size], options[:size], 180, 360)
+  end
+
+  colors = ['#eeb3b2', '#d5e8c5', '#bca7c7']
+  radius = 210
+
+  5.times do |i|
+    color = colors[i % colors.length]
+    options = { color: color, size: radius }
+    bridge.call(options)
+    radius = radius - (- 5*i + 30)
+  end
+  
+  options = { color: '#ffffff', size: 110 }
+  bridge.call(options) 
+end
