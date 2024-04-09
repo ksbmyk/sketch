d710957ef64d9604613c3216367e81defab5e08f
Miyuki Koshiba <ksbmyk@gmail.com>
2024-04-10 00:48:54 +0900

    sum coordinate

--- public/programs/20240411.rb
+++ public/programs/20240411.rb
@@ -8,40 +8,40 @@ def draw
   fill("#b62e2c")
   stroke("#b62e2c")
   rect(0, height-50, 30, height)
-  rect(30, height-40, 30/2, height)
-  rect(30+30/2, height-100, 30, height)
-  triangle(30+30/2+1, height-100,
-    30+30/2 +10,  height-100-5,
-    30+30/2+30-2, height-100)
+  rect(30, height-40, 15, height)
+  rect(45, height-100, 30, height)
+  triangle(46, height-100,
+    55,  height-105,
+    73, height-100)
   
-  rect(30+30/2+30, height-70, 30, height)
+  rect(75, height-70, 30, height)
   
   #docomo
-  rect(30+30/2+30+30-5, height-150, 30+10, height)
-  rect(30+30/2+30+30, height-170, 30, height)
-  rect(30+30/2+30+30+5, height-190, 20, height)
-  rect(30+30/2+30+30+5+5, height-200, 10, height)
-  rect(30+30/2+30+30+5+5+3, height-230, 4, height)
+  rect(100, height-150, 40, height)
+  rect(105, height-170, 30, height)
+  rect(110, height-190, 20, height)
+  rect(115, height-200, 10, height)
+  rect(118, height-230, 4, height)
   
-  rect(30+30/2+30+30+30, height-60, 20, height)
+  rect(135, height-60, 20, height)
   
-  rect(30+30/2+30+30+30+20, height-100, 15, height)
-  rect(30+30/2+30+30+30+20+15, height-90, 15, height)
-  rect(30+30/2+30+30+30+20+15+15, height-80, 15, height)
+  rect(155, height-100, 15, height)
+  rect(170, height-90, 15, height)
+  rect(185, height-80, 15, height)
   
-  rect(30+30/2+30+30+30+20+15+15+15, height-50, 30, height)
-  rect(30+30/2+30+30+30+20+15+15+15+30, height-45, 10, height)
-  rect(30+30/2+30+30+30+20+15+15+15+30+10, height-95, 20, height)
-  triangle(30+30/2+30+30+30+20+15+15+15+30+10+1, height-95,
-    30+30/2+30+30+30+20+15+15+15+30+10+10,  height-98,
-    30+30/2+30+30+30+20+15+15+15+30+10+20-1, height-95)
-  rect(30+30/2+30+30+30+20+15+15+15+30+10+8, height-110, 2, height)
+  rect(200, height-50, 30, height)
+  rect(230, height-45, 10, height)
+  rect(240, height-95, 20, height)
+  triangle(241, height-95,
+    250,  height-98,
+    259, height-95)
+  rect(248, height-110, 2, height)
   
-  rect(30+30/2+30+30+30+20+15+15+15+30+10+20, height-115, 40, height)
-  rect(30+30/2+30+30+30+20+15+15+15+30+10+20+5, height-120, 30, height)
-  rect(30+30/2+30+30+30+20+15+15+15+30+10+20+5+10, height-125, 10, height)
-  rect(30+30/2+30+30+30+20+15+15+15+30+10+20+5+10-2, height-125, 10+4, 2)
+  rect(260, height-115, 40, height)
+  rect(265, height-120, 30, height)
+  rect(275, height-125, 10, height)
+  rect(273, height-125, 14, 2)
   
-  rect(30+30/2+30+30+30+20+15+15+15+30+10+20+40, height-95, 10, height)
-  rect(30+30/2+30+30+30+20+15+15+15+30+10+20+40+10, height-85, 10, height)
+  rect(300, height-95, 10, height)
+  rect(310, height-85, 10, height)
 end
