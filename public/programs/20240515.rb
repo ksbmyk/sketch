5c0445954fb2d490f15f1f505a2213196c89698d
Miyuki Koshiba <ksbmyk@gmail.com>
2024-05-05 19:16:34 +0900

    refactor

--- public/programs/20240515.rb
+++ public/programs/20240515.rb
@@ -5,96 +5,70 @@ def setup
 end
 
 def draw
-  translate(width / 2, height / 2) # 原点をキャンパスの中心に
+  translate(width / 2, height / 2)
   rotate(PI/2)
-  strokeWeight(3)
 
-  # 枠
-  fill("#9cee60")
-  ellipse(0, 0, 120*2 +40, 120*2 +40)
- 
-  fill(255)
-  ellipse(0, 0, 120*2 +10, 120*2 +10) # 模様の外側
-  fill("#9cee60")
-  ellipse(0, 0, 120*2 -50, 120*2 -50) # 模様の内側
-  
-  fill(0)
-  noStroke
-  # 円周上の四角
-  rect_count = 10 #四角形の数
-  radius = 100 # 円の半径
-  interval_angle = TWO_PI / rect_count # 各四角形の間隔の角度
-
-  rect_count.times do |i|
-    angle = i * interval_angle # 四角形の角度
-    x = radius * cos(angle) # x座標を計算
-    y = radius * sin(angle) # y座標を計算
-    push()
-    translate(x, y) # 四角形の位置を移動
-    rotate(angle) # 四角形を円周に沿って回転
-    rectMode(CENTER)
-    if i.odd?
-    	rect(0, 0, 10, 10)
-    else
-      rect(0, 0-10, 10, 10)
-      rect(0, 0+10, 10, 10)
-    end
-    pop()
-  end
-  
-  # 円周上の四角2
-  rect_count = 10 #四角形の数
-  radius = 110 # 円の半径
-  interval_angle = TWO_PI / rect_count # 各四角形の間隔の角度
-
-  rect_count.times do |i|
-    angle = i * interval_angle # 四角形の角度
-    x = radius * cos(angle) # x座標を計算
-    y = radius * sin(angle) # y座標を計算
-    push()
-    translate(x, y) # 四角形の位置を移動
-    rotate(angle) # 四角形を円周に沿って回転
-    rectMode(CENTER)
-    if i.even?
-    	rect(0, 0, 10, 10)
-    else
-      rect(0, 0-10, 10, 10)
-      rect(0, 0+10, 10, 10)
-    end
-    pop()
-  end
-  
-  # 円周上の四角3
-  rect_count = 10 #四角形の数
-  radius = 120 # 円の半径
-  interval_angle = TWO_PI / rect_count # 各四角形の間隔の角度
-
-  rect_count.times do |i|
-    angle = i * interval_angle # 四角形の角度
-    x = radius * cos(angle) # x座標を計算
-    y = radius * sin(angle) # y座標を計算
-    push()
-    translate(x, y) # 四角形の位置を移動
-    rotate(angle) # 四角形を円周に沿って回転
-    rectMode(CENTER)
-    if i.odd?
-    	rect(0, 0, 10, 10)
-    else
-      rect(0, 0-10, 10, 10)
-      rect(0, 0+10, 10, 10)
-    end
-    pop()
-  end
-  
-  # 枠
-  stroke(0)
-  fill(255)
-  ellipse(0, 0, 120*2 -70, 120*2 -70)
-
+  draw_frame
+  draw_minsa
   draw_ruby
 end
 
+def draw_frame
+  strokeWeight(3)
+  fill("#9cee60")
+  ellipse(0, 0, 280)
+  fill(255)
+  ellipse(0, 0, 250)
+  fill("#9cee60")
+  ellipse(0, 0, 190)
+  fill(255)
+  ellipse(0, 0, 170)
+end
+
+def draw_minsa
+  fill(0)
+  noStroke
+
+  # 円周上の四角
+  fill(0)
+  noStroke
+  rect_count = 10 #四角形の数
+
+  3.times do |t|
+    radius = 100 + 10 * t # 円の半径
+    interval_angle = TWO_PI / rect_count # 各四角形の間隔の角度
+
+    rect_count.times do |i|
+      angle = i * interval_angle # 四角形の角度
+      x = radius * cos(angle)
+      y = radius * sin(angle)
+      push
+      translate(x, y)
+      rotate(angle) # 四角形を円周に沿って回転
+      rectMode(CENTER)
+
+      if t.even?
+        if i.odd?
+          rect(0, 0, 10, 10)
+        else
+          rect(0, 0-10, 10, 10)
+          rect(0, 0+10, 10, 10)
+        end
+      else
+        if i.even?
+          rect(0, 0, 10, 10)
+        else
+          rect(0, 0-10, 10, 10)
+          rect(0, 0+10, 10, 10)
+        end
+      end
+      pop
+    end
+  end
+end
+
 def draw_ruby
+  stroke(0)
   fill("#ec6158")
 
   beginShape()
