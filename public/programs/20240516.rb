412842bc73473efac4584ab48f434a8e0f215943
Miyuki Koshiba <ksbmyk@gmail.com>
2024-05-06 22:11:10 +0900

    add frame method

--- public/programs/20240516.rb
+++ public/programs/20240516.rb
@@ -1,26 +1,31 @@
 def setup
-  createCanvas(700, 700)
+  size = min(windowWidth, 1000)
+  createCanvas(size, size)
   background("gray")
   noLoop
 end
 
 def draw
-  
-  # フレーム
-  stroke(0)
-  rect_size = 80
-  rect_weight = 2
-  strokeWeight(rect_weight)
-  
-  fill("#9cee60")
-  rect(rect_weight, rect_weight, width, rect_size)
-  rect(width - rect_weight - rect_size , rect_weight, rect_size, height)
-  rect(rect_weight, height - rect_weight - rect_size, width, rect_size)
-  rect(rect_weight, rect_weight, rect_size, height)
-  
-  fill("#4b92f7")
-  rect(rect_weight, rect_weight, rect_size)
-  rect(width - rect_weight - rect_size , rect_weight, rect_size)
-  rect(rect_weight, height - rect_weight - rect_size, rect_size)
-  rect(width - rect_weight - rect_size , height - rect_weight - rect_size, rect_size)
+  frame
+end
+
+def frame
+    # TODO フレームの色パターン追加
+    # フレーム
+    stroke(0)
+    rect_size = 80
+    rect_weight = 2
+    strokeWeight(rect_weight)
+    
+    fill("#9cee60")
+    rect(rect_weight, rect_weight, width, rect_size)
+    rect(width - rect_weight - rect_size , rect_weight, rect_size, height)
+    rect(rect_weight, height - rect_weight - rect_size, width, rect_size)
+    rect(rect_weight, rect_weight, rect_size, height)
+    
+    fill("#4b92f7")
+    rect(rect_weight, rect_weight, rect_size)
+    rect(width - rect_weight - rect_size , rect_weight, rect_size)
+    rect(rect_weight, height - rect_weight - rect_size, rect_size)
+    rect(width - rect_weight - rect_size , height - rect_weight - rect_size, rect_size)
 end
