50abfd7051151db1375df13db08a8703dca8f6a0
Miyuki Koshiba <ksbmyk@gmail.com>
2024-05-11 00:21:43 +0900

    add sea

--- public/programs/20240516.rb
+++ public/programs/20240516.rb
@@ -1,11 +1,13 @@
 def setup
   size = min(windowWidth, 1000)
   createCanvas(size, size)
+  angleMode(DEGREES)
   background("gray")
   noLoop
 end
 
 def draw
+  sea
   frame
 end
 
@@ -28,4 +30,34 @@ def frame
     rect(width - rect_weight - rect_size , rect_weight, rect_size)
     rect(rect_weight, height - rect_weight - rect_size, rect_size)
     rect(width - rect_weight - rect_size , height - rect_weight - rect_size, rect_size)
+end
+
+
+def sea
+  noStroke
+  drawingContext.shadowColor = color(255)
+  drawingContext.shadowBlur = 50
+
+  x_value = width / 100
+  y_value = height / 15
+  freq = map(100, 0, width, 1, 5)
+  vel = map(200, 0, height, 1, 50)
+
+  (height/10 * 4..height/10 * 8).step(y_value) do |y|
+	 if y < height/10 * 7
+      fill(0, random(100, 150), random(200, 250))
+    else
+      fill(220, 221, 178)
+    end
+    beginShape()
+
+    (0..width).step(x_value) do |x|
+      a = 200
+      b = sin((y + x + a) * freq) * vel
+      vertex(x, b + y)
+    end
+    vertex(width, height)
+    vertex(0, height)
+    endShape(CLOSE)
+  end
 end
