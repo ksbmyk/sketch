af9ce1ef39ac0c4f47da1e0bd209419eaaa48cac
Miyuki Koshiba <ksbmyk@gmail.com>
2024-06-01 17:02:31 +0900

    色調整

--- public/programs/20240601.rb
+++ public/programs/20240601.rb
@@ -2,12 +2,15 @@ def setup
   createCanvas(700, 700)
   @circles = []
   @rate = 0.08
+  frameRate(40)
+  drawingContext.shadowColor = color(220)
+  drawingContext.shadowBlur = 50
+
 end
 
 def draw
   background(0)
   noStroke
-
   if rand < @rate
     @circles << Circle.new(rand(0..width), rand(0..height))
   end
@@ -17,25 +20,28 @@ def draw
     circle.display
     @circles.reject! { |c| c.finished? }
   end
+
 end
 
 class Circle
-  attr_accessor :x, :y, :radius, :alpha
+  attr_accessor :x, :y, :radius, :alpha, :c
 
   def initialize(x, y)
     @x = x
     @y = y
     @radius = 0
     @alpha = 255
+    @c = rand(100..255)
   end
 
   def update
     self.radius = radius + 1
-    self.alpha = alpha - 1
+    self.alpha = alpha - 2
   end
 
   def display
-    fill(0, 0, 255, alpha)
+    fill(0, 0, c, alpha)
+
     ellipse(x, y, radius * 2)
   end
 
