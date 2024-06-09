31a93ebe3697d9aa70634f810953dcdf56af8c06
Miyuki Koshiba <ksbmyk@gmail.com>
2024-06-09 21:36:50 +0900

    add arc and rect

--- public/programs/20240609.rb
+++ public/programs/20240609.rb
@@ -7,6 +7,7 @@ def setup
   rectMode(CENTER)
   noStroke
   noLoop
+  blendMode(SCREEN)
 end
 
 def draw
@@ -16,12 +17,20 @@ def draw
     while y < height do
       alpha = map(y, 0, height, 50, 255)
       fill(100, 150, 200, alpha)
+
       ellipse(x + $size / 2, y + $size / 2, rand(3..$size * 0.8))
+
       push
       translate(x + $size / 2, y + $size / 2)
       rotate(PI / 4)
       rect(0, 0, rand(3..$size * 0.7))
       pop
+
+      rect(x + $size / 2, y + $size / 2, rand(3..$size * 0.5))
+
+      r = rand(3..$size * 0.5)
+      arc(x + $size / 2, y + $size / 2, r, r, PI/2*rand(0..3), PI/2*rand(0..3))
+
       y += $size 
     end
     x += $size
