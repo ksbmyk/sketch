121b181889cfdc2ee124b8e7b3c13f36dae7f796
Miyuki Koshiba <ksbmyk@gmail.com>
2024-06-09 11:29:20 +0900

    add color

--- public/programs/20240608.rb
+++ public/programs/20240608.rb
@@ -18,15 +18,16 @@ def create_new_circle
   x = rand(0..width)
   y = rand(0..height)
   r = rand(5..50)
-
+  c = color(map(x, 0, width, 100, 0), map(y, 0, height, 255, 0), 255)
+  
   return nil if $circles.any? { |circle| dist(x, y, circle[:x], circle[:y]) < r + circle[:r] }
 
-  { x: x, y: y, r: r}
+  { x: x, y: y, r: r, c: c }
 end
 
 def draw_circles
   $circles.each do |circle|
-    fill(0)
+    fill(circle[:c])
     ellipse(circle[:x], circle[:y], circle[:r] * 2)
   end
 end
