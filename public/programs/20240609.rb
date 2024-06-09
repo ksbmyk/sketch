f800ea23d75d7303d006b969cbeefc7fdd0f43a0
Miyuki Koshiba <ksbmyk@gmail.com>
2024-06-09 20:50:06 +0900

    rotate

--- public/programs/20240609.rb
+++ public/programs/20240609.rb
@@ -17,7 +17,11 @@ def draw
       alpha = map(y, 0, height, 50, 255)
       fill(100, 150, 200, alpha)
       ellipse(x + $size / 2, y + $size / 2, rand(3..$size * 0.8))
-      rect(x + $size / 2, y + $size / 2, rand(3..$size * 0.8))
+      push
+      translate(x + $size / 2, y + $size / 2)
+      rotate(PI / 4)
+      rect(0, 0, rand(3..$size * 0.7))
+      pop
       y += $size 
     end
     x += $size
