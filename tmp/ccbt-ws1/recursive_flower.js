4d6716372d2ce1e67e61f209d3e8133b3d41843b
Miyuki Koshiba <ksbmyk@gmail.com>
2024-08-21 14:43:44 +0900

    分割可能にした

--- tmp/ccbt-ws1/recursive_flower.js
+++ tmp/ccbt-ws1/recursive_flower.js
@@ -1,39 +1,47 @@
-let  n = 5;
+let n = 5;
 let r1, r2;
+let size = 100;
+let grid = 4
+
 function setup() {
-  createCanvas(300, 300);
+  createCanvas(size * grid, size * grid);
   background(0);
   frameRate(30);
 }
 
 function draw() {
   
-if (frameCount % 30 === 0){
-  background(0);
-  [r1, r2] = getUniqueRandomPair(3, 6);
-  
-  stroke(255);
-  strokeWeight(2);
-  noFill();
-  pattern(n, r1);
+  if (frameCount % 20 === 0){
+    background(0);
 
-  stroke(random(0,100), random(100, 255), 255);
-  strokeWeight(1);
-  pattern(n, r2);
-  
-  if (n === 7){
-    n = 5;
-  }else{
-    n = n + 1;
+    for (let x = 0; x < width; x += size) {
+      for (let y = 0; y < height; y += size) {
+
+      [r1, r2] = getUniqueRandomPair(3, 6);
+      stroke(255);
+      strokeWeight(2);
+      noFill();
+      pattern(x, y, n, r1);
+
+      stroke(random(0,100), random(100, 255), 255);
+      strokeWeight(1);
+      pattern(x, y, n, r2);
+
+      n = floor(random(5, 9));
+      }
+    }
+
+    // if (n === 8){
+    //   n = 5;
+    // }else{
+    //   n = n + 1;
+    // }
   }
 }
-  
-  //n = floor(random(5,9));
-}
 
 
-function pattern(n, r) {
-  geometric(width/2, width/2, width, n, r);
+function pattern(x, y, n, r) {
+  geometric(x+size/2, y+size/2, size, n, r);
 }
 
 function geometric(x, y, radius, sides, depth) {
