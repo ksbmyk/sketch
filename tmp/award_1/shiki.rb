622a18a52a96d9c71aa3c8c76ab96c9bd62e7f35
Miyuki Koshiba <ksbmyk@gmail.com>
2024-08-23 20:50:42 +0900

    四季調整

--- tmp/award_1/shiki.rb
+++ tmp/award_1/shiki.rb
@@ -1,84 +1,60 @@
-$falling_objects = []
-$num_objects = 10
-$seasons = ["spring", "summer", "autumn", "winter"]
-$season_colors = {
-  "spring" => [0, 0, 0],       # 春色 (ピンク)
-  "summer" => [255, 182, 193], # 夏色 (空色)
-  "autumn" => [135, 206, 250], # 秋色 (オレンジ)
-  "winter" => [255, 204, 0]    # 冬色 (黒)
-}
-
-$season_objects = {
-  "spring" => { char: '🌸', rotates: true },
-  "summer" => { char: '💧', rotates: false },
-  "autumn" => { char: '🍁', rotates: true },
-  "winter" => { char: '❄️', rotates: true }
-}
-
-$current_season_index = 0
-$next_season_index = 1
-$lerp_amount = 0
-$total_frames_per_season = 500
-
 def setup
   createCanvas(600, 600)
   textAlign(CENTER, CENTER)
-  initialize_objects
+  @falling_objects = []
+  @seasons = [:spring, :summer, :autumn, :winter]
+  @season_colors = {
+    spring: [0, 0, 0], 
+    summer: [255, 182, 193], 
+    autumn: [135, 206, 250], 
+    winter:  [255, 204, 0] 
+  }
+  @season_objects = {
+    spring:  { char: '🌸', rotates: true },
+    summer:  { char: '💧', rotates: false },
+    autumn:  { char: '🍁', rotates: true },
+    winter: { char: '❄️', rotates: true }
+  }
+  @current_season_index = 0
+  @next_season_index = 1
+  @lerp_amount = 0
+  @total_frames_per_season = 500
+  initialize_objects(@current_season_index)
+  frameRate(60)
 end
 
 def draw
   textSize(32)
-  # 背景色の設定
-  current_season_color = $season_colors[$seasons[$current_season_index]]
-  next_season_color = $season_colors[$seasons[$next_season_index]]
+  current_season_color = @season_colors[@seasons[@current_season_index]]
+  next_season_color = @season_colors[@seasons[@next_season_index]]
 
-  #adjusted_lerp_amount = $lerp_amount
-
-  #bg_color = lerpColor(color(*current_season_color), color(*next_season_color), adjusted_lerp_amount)
-  bg_color = lerpColor(color(*current_season_color), color(*next_season_color), $lerp_amount)
+  bg_color = lerpColor(color(*current_season_color), color(*next_season_color), @lerp_amount)
   background(bg_color)
 
-  # オブジェクトを表示
-  $falling_objects.reject! do |obj|
+  @falling_objects.reject! do |obj|
     obj.update
     obj.display
-    obj.is_out_of_screen
+    obj.is_out_of_screen?
   end
 
-  # 徐々に次の季節へ
-  $lerp_amount += 1.0 / $total_frames_per_season
-  if $lerp_amount >= 1
-    $lerp_amount = 0
-    $current_season_index = ($current_season_index + 1) % $seasons.length
-    $next_season_index = ($current_season_index + 1) % $seasons.length
-    add_new_season_objects
+  @lerp_amount += 1.0 / @total_frames_per_season
+  if @lerp_amount >= 1
+    @lerp_amount = 0
+    @current_season_index = (@current_season_index + 1) % @seasons.length
+    @next_season_index = (@current_season_index + 1) % @seasons.length
+    initialize_objects(@current_season_index)
   end
 end
 
-def initialize_objects
-  season_info = $season_objects[$seasons[$current_season_index]]
-  
-  # $num_objects.times do
+def initialize_objects(season_index)
+  season_info = @season_objects[@seasons[season_index]]
   10.times do
     x = rand(0..width)
     y = rand(-height..0)
-    $falling_objects << FallingObject.new(x, y, season_info[:char], season_info[:rotates])
+    @falling_objects << FallingObject.new(x, y, season_info[:char], season_info[:rotates])
   end
 end
 
-def add_new_season_objects
-  season_info = $season_objects[$seasons[$current_season_index]]
-
-  # 新しい季節のオブジェクトを追加（古いオブジェクトは落ち切るまで残る）
-  # $num_objects.times do
-  10.times do
-    x = rand(0..width)
-    y = rand(-height..0)
-    $falling_objects << FallingObject.new(x, y, season_info[:char], season_info[:rotates])
-  end
-end
-
-# FallingObjectクラスを定義
 class FallingObject
   attr_accessor :x, :y, :char, :rotates, :rotation, :speed
 
@@ -87,7 +63,7 @@ class FallingObject
     @y = y
     @char = char
     @rotates = rotates
-    @rotation = rotates ? rand(0..TWO_PI) : 0  # 回転しない場合は0に固定
+    @rotation = rotates ? rand(0..TWO_PI) : 0
     @speed = rand(2..5)
   end
 
@@ -99,13 +75,12 @@ class FallingObject
   def display
     push
     translate(@x, @y)
-    rotate(@rotation)  # rotatesがfalseならrotationは0なので回転しない
+    rotate(@rotation)
     text(@char, 0, 0)
     pop
   end
-  
-  # オブジェクトが画面外に出たかどうかをチェックする
-  def is_out_of_screen
+
+  def is_out_of_screen?
     @y > height
   end
 end
