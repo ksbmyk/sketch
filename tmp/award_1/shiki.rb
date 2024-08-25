d66c85ac48cbabbd1333482d72ffcc1191a10931
Miyuki Koshiba <ksbmyk@gmail.com>
2024-08-25 18:06:59 +0900

    refactor

--- tmp/award_1/shiki.rb
+++ tmp/award_1/shiki.rb
@@ -1,76 +1,75 @@
 def setup
   createCanvas(600, 600)
   textAlign(CENTER, CENTER)
-  @flakes = []
-  @flake_num = 100
-  @seasons = [:spring, :summer, :autumn, :winter]
-  @season_colors = {
-  spring: [255, 182, 193],
-  summer: [135, 206, 250],
-  autumn: [204, 163, 0],
-  winter: [0, 31, 63]
-  }
-  @season_params = {
-    spring: { char: '✿', rotates: true, size: 32, color: [255, 105, 180]},
-    summer: { char: ';', rotates: false, size: 32, color: [30, 144, 255]},
-    autumn: { char: '♣', rotates: true, size: 32, color: [204, 85, 0]},
-    winter: { char: '*', rotates: false, size: 50, color: [255, 255, 255]}
-  }
-  @current_season_index = 0
-  @next_season_index = 1
-  @lerp_ratio = 0
-  @total_frames_per_season = 600
-  initialize_objects(@current_season_index)
+  initialize_data
+  initialize_flakes
   frameRate(60)
 end
 
 def draw
-  # 色変化のタイミングを調整するための変数
-  transition_threshold = 0.5 # 0.5までのlerp_ratioで色を維持する
+  update_background_color
+  handle_season_transition
+  update_and_display_flakes
+end
 
-  # 季節を変えるコード
-  @lerp_ratio += 1.0 / @total_frames_per_season
+private
+
+def initialize_data
+  @flakes = []
+  @flake_num = 100
+  @seasons = [:spring, :summer, :autumn, :winter]
+  @season_data = {
+    spring: { char: '✿', rotates: true, size: 32, color: [255, 105, 180], bg_color: [255, 182, 193] },
+    summer: { char: ';', rotates: false, size: 32, color: [30, 144, 255], bg_color: [135, 206, 250] },
+    autumn: { char: '♣', rotates: true, size: 32, color: [204, 85, 0], bg_color: [204, 163, 0] },
+    winter: { char: '*', rotates: false, size: 50, color: [255, 255, 255], bg_color: [0, 31, 63] }
+  }
+  @current_season = :spring
+  @next_season = :summer
+  @lerp_ratio = 0.0
+  @season_frame_count = 600
+end
+
+def update_background_color
+  transition_threshold = 0.5  # 色変化の開始点
+  adjusted_lerp_ratio = @lerp_ratio < transition_threshold ? 0 : map(@lerp_ratio, transition_threshold, 1, 0, 1)
   
-  # # 序盤は現在の色を維持し、後半で次の色に変化
-  if (@lerp_ratio < transition_threshold)
-    adjustedlerp_ratio = 0;  # 色を維持
-  else
-    adjustedlerp_ratio = map(@lerp_ratio, transition_threshold, 1, 0, 1)  # 徐々に変化
-  end
+  current_bg_color = @season_data[@current_season][:bg_color]  # 現在の季節の背景色
+  next_bg_color = @season_data[@next_season][:bg_color]  # 次の季節の背景色
+  bg_color = lerpColor(color(*current_bg_color), color(*next_bg_color), adjusted_lerp_ratio)  # 背景色の補間
   
-  # 背景色の設定
-  current_season_color = @season_colors[@seasons[@current_season_index]]
-  next_season_color = @season_colors[@seasons[@next_season_index]]
-  bg_color = lerpColor(color(*current_season_color), color(*next_season_color), adjustedlerp_ratio)
   background(bg_color)
+  @lerp_ratio += 1.0 / @season_frame_count
+end
 
-  # 季節が変わるタイミング
+def handle_season_transition
   if @lerp_ratio >= 1
     @lerp_ratio = 0
-    @current_season_index = (@current_season_index + 1) % @seasons.length
-    @next_season_index = (@current_season_index + 1) % @seasons.length
-    initialize_objects(@current_season_index)
-  end
-
-  # 表示
-  @flakes.reject! do |obj|
-    obj.update
-    obj.display
-    obj.is_out_of_screen?
+    @current_season = @next_season
+    @next_season = @seasons[(@seasons.index(@current_season) + 1) % @seasons.length]
+    initialize_flakes
   end
 end
 
-def initialize_objects(season_index)
-  season_info = @season_params[@seasons[season_index]]
+def initialize_flakes
+  season_attributes = @season_data[@current_season]
   @flake_num.times do
-    x = rand(0..width)
+    x = rand(width)
     y = rand(-height..0)
-    @flakes << Flake.new(x, y, season_info[:char], season_info[:color], season_info[:size], season_info[:rotates])
+    @flakes << Flake.new(x, y, season_attributes[:char], season_attributes[:color], season_attributes[:size], season_params[:rotates])
+  end
+end
+
+def update_and_display_flakes
+  @flakes.reject! do |flake|
+    flake.update
+    flake.display
+    flake.out_of_screen?
   end
 end
 
 class Flake
-  attr_accessor :x, :y, :char, :rotates, :rotation, :speed
+  attr_reader :x, :y, :char, :color, :size
 
   def initialize(x, y, char, color, size, rotates)
     @x, @y, @char, @color, @size, @rotates = x, y, char, color, size, rotates
@@ -94,7 +93,7 @@ class Flake
     pop
   end
 
-  def is_out_of_screen?
+  def out_of_screen?
     @y > height
   end
 end
