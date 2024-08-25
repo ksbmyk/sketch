6cca2231900166a4d2177628fff09fde7cf26395
Miyuki Koshiba <ksbmyk@gmail.com>
2024-08-25 17:19:04 +0900

    refactor

--- tmp/award_1/shiki.rb
+++ tmp/award_1/shiki.rb
@@ -1,8 +1,8 @@
 def setup
   createCanvas(600, 600)
   textAlign(CENTER, CENTER)
-  @falling_objects = []
-  @num_objects = 100
+  @flakes = []
+  @flake_num = 100
   @seasons = [:spring, :summer, :autumn, :winter]
   @season_colors = {
   spring: [255, 182, 193],
@@ -10,7 +10,7 @@ def setup
   autumn: [204, 163, 0],
   winter: [0, 31, 63]
   }
-  @season_objects = {
+  @season_params = {
     spring: { char: '✿', rotates: true, size: 32, color: [255, 105, 180]},
     summer: { char: ';', rotates: false, size: 32, color: [30, 144, 255]},
     autumn: { char: '♣', rotates: true, size: 32, color: [204, 85, 0]},
@@ -18,7 +18,7 @@ def setup
   }
   @current_season_index = 0
   @next_season_index = 1
-  @lerp_amount = 0
+  @lerp_ratio = 0
   @total_frames_per_season = 600
   initialize_objects(@current_season_index)
   frameRate(60)
@@ -26,34 +26,34 @@ end
 
 def draw
   # 色変化のタイミングを調整するための変数
-  transition_threshold = 0.5 # 0.5までのlerp_amountで色を維持する
+  transition_threshold = 0.5 # 0.5までのlerp_ratioで色を維持する
 
   # 季節を変えるコード
-  @lerp_amount += 1.0 / @total_frames_per_season
+  @lerp_ratio += 1.0 / @total_frames_per_season
   
   # # 序盤は現在の色を維持し、後半で次の色に変化
-  if (@lerp_amount < transition_threshold)
-    adjustedLerp_amount = 0;  # 色を維持
+  if (@lerp_ratio < transition_threshold)
+    adjustedlerp_ratio = 0;  # 色を維持
   else
-    adjustedLerp_amount = map(@lerp_amount, transition_threshold, 1, 0, 1)  # 徐々に変化
+    adjustedlerp_ratio = map(@lerp_ratio, transition_threshold, 1, 0, 1)  # 徐々に変化
   end
   
   # 背景色の設定
   current_season_color = @season_colors[@seasons[@current_season_index]]
   next_season_color = @season_colors[@seasons[@next_season_index]]
-  bg_color = lerpColor(color(*current_season_color), color(*next_season_color), adjustedLerp_amount)
+  bg_color = lerpColor(color(*current_season_color), color(*next_season_color), adjustedlerp_ratio)
   background(bg_color)
 
   # 季節が変わるタイミング
-  if @lerp_amount >= 1
-    @lerp_amount = 0
+  if @lerp_ratio >= 1
+    @lerp_ratio = 0
     @current_season_index = (@current_season_index + 1) % @seasons.length
     @next_season_index = (@current_season_index + 1) % @seasons.length
     initialize_objects(@current_season_index)
   end
 
   # 表示
-  @falling_objects.reject! do |obj|
+  @flakes.reject! do |obj|
     obj.update
     obj.display
     obj.is_out_of_screen?
@@ -61,11 +61,11 @@ def draw
 end
 
 def initialize_objects(season_index)
-  season_info = @season_objects[@seasons[season_index]]
-  @num_objects.times do
+  season_info = @season_params[@seasons[season_index]]
+  @flake_num.times do
     x = rand(0..width)
     y = rand(-height..0)
-    @falling_objects << Flake.new(x, y, season_info[:char], season_info[:color], season_info[:size], season_info[:rotates])
+    @flakes << Flake.new(x, y, season_info[:char], season_info[:color], season_info[:size], season_info[:rotates])
   end
 end
 
