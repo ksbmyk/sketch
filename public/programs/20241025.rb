4cc233ebb4e3b1cd490c8ecd443831955bb1b55d
Miyuki Koshiba <ksbmyk@gmail.com>
2024-10-12 22:22:42 +0900

    円弧追加

--- public/programs/20241025.rb
+++ public/programs/20241025.rb
@@ -1,4 +1,5 @@
 def setup
+  angleMode(DEGREES)
   @size = 100
   @num = 5
   createCanvas(@size * @num, @size * @num)
@@ -12,6 +13,13 @@ def draw
       c = (i + j).even? ? "#fe4053" : "#00c5da"
       fill(c)
       rect(i * @size, j * @size, @size)
+
+      large_arc(i * @size + @size, j * @size + @size)
     end
   end
+end
+
+def large_arc(x, y)
+  fill("#ffffff")
+  arc(x, y, @size * 2, @size * 2, 180, 270)
 end
