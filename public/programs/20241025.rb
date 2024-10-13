0f90acf94b6169b54ca6cd01ae5f170107b229cc
Miyuki Koshiba <ksbmyk@gmail.com>
2024-10-13 12:57:22 +0900

    円弧の色を違い違いにする

--- public/programs/20241025.rb
+++ public/programs/20241025.rb
@@ -3,6 +3,7 @@ def setup
   @size = 100
   @num = 5
   createCanvas(@size * @num, @size * @num)
+  background("#aaaaaa")
   noLoop
 end
 
@@ -11,15 +12,12 @@ def draw
   @num.times do |i|
     @num.times do |j|
       c = (i + j).even? ? "#fe4053" : "#00c5da"
-      fill(c)
-      rect(i * @size, j * @size, @size)
-
-      large_arc(i * @size + @size, j * @size + @size)
+      large_arc(i * @size + @size, j * @size + @size, c)
     end
   end
 end
 
-def large_arc(x, y)
-  fill("#ffffff")
+def large_arc(x, y, color)
+  fill(color)
   arc(x, y, @size * 2, @size * 2, 180, 270)
 end
