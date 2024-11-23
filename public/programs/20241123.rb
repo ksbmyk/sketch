c56532a44e7f87f8f2383b088cd7d60094b994a4
Miyuki Koshiba <ksbmyk@gmail.com>
2024-11-23 17:43:05 +0900

    change cream soda

--- public/programs/20241123.rb
+++ public/programs/20241123.rb
@@ -1,23 +1,42 @@
 def setup
-  createCanvas(700, 700)
-  @bubbles = 150.times.map { Bubble.new(rand(0..width), rand(0..height)) }
+  createCanvas(600, 600)
+  $glass = createGraphics(200, 300)
+  @bubbles = 100.times.map { Bubble.new(rand(0..200), rand(0..300)) }
 end
 
 def draw
-  background(0, 51, 102)
+  background(0, 128, 128)
 
+  $glass.clear
   @bubbles.each do |bubble|
     bubble.update
     bubble.display
   end
+
+  # アイス
+  noStroke
+  fill(248, 244, 227)
+  arc(300, 300, 200, 200, PI, TWO_PI);
+
+  # グラス
+  image($glass, 200, 300)
+  # チェリー
+  fill(200, 0, 0)
+  ellipse(380, 280, 50)
+
+  # グラス
+  fill(255, 255, 255, 50)
+  stroke(255)
+  strokeWeight(3)
+  rect(200, 300, 200, 300)
 end
 
 class Bubble
   def initialize(x, y)
     @x = x
     @y = y
-    @size = rand(5..30)
-    @speed = rand(1..3.5)
+    @size = rand(5..15)
+    @speed = rand(1..3)
     @color = color(255, 255, 255, rand(100..200)) # 半透明
   end
 
@@ -25,16 +44,19 @@ class Bubble
     # 下から上へ
     @y -= @speed
 
-    # 画面外に出たら再度位置を設定
-    if @y < 0
-      @y = height
-      @x = rand(0..width)
+    # グラスの範囲を超えないようにする
+    if @y - @size / 2 < 0
+      @y = 300 - @size / 2 # 上端を超えないように再設定
+    end
+
+    if @y + @size / 2 > 300
+      @y = @size / 2 # 下端を超えないように再設定
     end
   end
 
   def display
-    fill(@color)
-    noStroke
-    ellipse(@x, @y, @size)
+    $glass.fill(@color)
+    $glass.noStroke
+    $glass.ellipse(@x, @y, @size)
   end
 end
