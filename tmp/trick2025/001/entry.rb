968341c2ecb5920ca70badf1513130e484f974ec
Miyuki Koshiba <ksbmyk@gmail.com>
2025-01-01 14:49:43 +0900

    add base

---
+++ tmp/trick2025/001/entry.rb
@@ -0,0 +1,13 @@
+def shooting_star(width, height, speed)
+  chars = [' ', '.', '*', '✨']
+  (1..height).each do |y|
+    system('clear')
+    (1..y).each do |line|
+      puts " " * (line % width) + chars.sample
+    end
+    puts " " * (y % width) + "🌟"
+    sleep(speed)
+  end
+end
+
+shooting_star(50, 20, 0.1)
