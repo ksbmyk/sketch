8afb2b3cbcc43388cc38af65d9ec3bc9afc7a84d
Miyuki Koshiba <ksbmyk@gmail.com>
2025-01-08 23:39:47 +0900

    add base

---
+++ public/programs/20250108.rb
@@ -0,0 +1,16 @@
+def setup
+  createCanvas(700, 700)
+  background(255)
+end
+
+def draw
+  stroke(0, 0, 200, 100)
+  x = rand(1..width - 1)
+  y = rand(1..height - 1)
+  if frameCount.to_i.odd?
+    line(x, y, x + 50, y)
+  else
+    line(x, y, x, y + 50)
+  end
+  noLoop if frameCount == 1000000
+end
