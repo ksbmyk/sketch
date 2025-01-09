38346aedc342e4314f3529e9f26b2ef31014d20f
Miyuki Koshiba <ksbmyk@gmail.com>
2025-01-09 20:11:39 +0900

    微妙に緑の色を変える

--- public/programs/20250109.rb
+++ public/programs/20250109.rb
@@ -10,9 +10,9 @@ def draw
   (0..height).step(grid_size).with_index do |y, yi|
     (0..width).step(grid_size).with_index do |x, xi|
       if (xi + yi).even?
-        fill(70 , 170, 70)
+        fill(70 + rand(-10..10), 170 + rand(-10..10), 70 + rand(-10..10))
       else
-        fill(40, 130 , 40)
+        fill(40 + rand(-10..10), 130 + rand(-10..10), 40 + rand(-10..10))
       end
 
       rect(x, y, grid_size, grid_size)
