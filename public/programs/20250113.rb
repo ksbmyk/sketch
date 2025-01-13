eaf3bafaf1f3758958c0c9d53902aed63837ba66
Miyuki Koshiba <ksbmyk@gmail.com>
2025-01-13 21:59:58 +0900

    add base

---
+++ public/programs/20250113.rb
@@ -0,0 +1,23 @@
+def setup
+  createCanvas(700, 700)
+  noStroke
+end
+
+def draw
+  background(0)
+  rows = 12
+  cols = 12
+  size = width / cols
+
+  rows.times do |y|
+    cols.times do |x|
+      h = sin(frameCount * 0.02 + (x + y) * 0.1) * 50
+      fill(100, 200, map(h, -50, 50, 0, 255))
+
+      # 1: x * size,            y * size,
+      # 2: x * size + size / 2, y * size + h,
+      # 3: x * size + size,     y * size
+      triangle(x * size, y * size, x * size + size / 2, y * size + h, x * size + size, y * size)
+    end
+  end
+end
