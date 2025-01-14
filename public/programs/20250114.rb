d16366f10416e4f01c5e7d8f11b06a2b76066b88
Miyuki Koshiba <ksbmyk@gmail.com>
2025-01-14 22:20:57 +0900

    change size

--- public/programs/20250114.rb
+++ public/programs/20250114.rb
@@ -1,5 +1,5 @@
 def setup
-  createCanvas(400, 400)
+  createCanvas(700, 700)
   noStroke
   frameRate(10)
 end
@@ -10,10 +10,10 @@ def draw
     fill(rand > 0.5 ? 0 : 255)
     x1 = rand(width)
     y1 = rand(height)
-    x2 = x1 + rand(-50..50)
-    y2 = y1 + rand(-50..50)
-    x3 = x1 + rand(-50..50)
-    y3 = y1 + rand(-50..50)
+    x2 = x1 + rand(-100..100)
+    y2 = y1 + rand(-100..100)
+    x3 = x1 + rand(-100..100)
+    y3 = y1 + rand(-100..100)
     triangle(x1, y1, x2, y2, x3, y3)
   end
 end
