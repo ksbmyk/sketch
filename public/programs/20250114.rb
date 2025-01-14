f8f5c1d1ce6182c213395045a9664d1eba832139
Miyuki Koshiba <ksbmyk@gmail.com>
2025-01-14 22:22:04 +0900

    中央寄せ

--- public/programs/20250114.rb
+++ public/programs/20250114.rb
@@ -8,8 +8,8 @@ def draw
   background(255)
   100.times do
     fill(rand > 0.5 ? 0 : 255)
-    x1 = rand(width)
-    y1 = rand(height)
+    x1 = width / 2 + rand(-100..100)
+    y1 = height / 2 + rand(-100..100)
     x2 = x1 + rand(-100..100)
     y2 = y1 + rand(-100..100)
     x3 = x1 + rand(-100..100)
