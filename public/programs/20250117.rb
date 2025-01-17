d972e14a3c41ee253eb188c96c17200a94c255ff
Miyuki Koshiba <ksbmyk@gmail.com>
2025-01-17 20:20:50 +0900

    小数を扱えるようにする

--- public/programs/20250117.rb
+++ public/programs/20250117.rb
@@ -1,6 +1,3 @@
-# GENUARY 2025 jan17 "What happens if pi=4?"
-# https://genuary.art/prompts
-
 def setup
   createCanvas(600, 600)
   noFill
@@ -8,15 +5,14 @@ end
 
 def draw
   translate(width / 2, height / 2)
-
+  # p = PI
+  p = 4
   10.times do |i|
-    # angle = frameCount * (0.01 + i * 0.001) + i * PI / 5
-    angle = frameCount * (0.01 + i * 0.001) + i * 4 / 5
+    angle = frameCount * (0.01 + i * 0.001) + i * p / 5
     radius = 50 + i * 10
     stroke(255 - i * 20, 150, 255, 50)
     beginShape
-    # (0...TWO_PI).step(PI / 6) do |a|
-    (0...4 * 2).step(4 / 6) do |a|
+     (0...p * 2).step(p.fdiv(6)) do |a|
       x = cos(a + angle) * radius
       y = sin(a + angle) * radius
       vertex(x, y)
