c4276a4cf9eac645f7c0e0d028eba008a6cdf96c
Miyuki Koshiba <ksbmyk@gmail.com>
2025-01-17 19:42:22 +0900

    add "if pi = 4"

--- public/programs/20250117.rb
+++ public/programs/20250117.rb
@@ -10,11 +10,13 @@ def draw
   translate(width / 2, height / 2)
 
   10.times do |i|
-    angle = frameCount * (0.01 + i * 0.001) + i * PI / 5
+    # angle = frameCount * (0.01 + i * 0.001) + i * PI / 5
+    angle = frameCount * (0.01 + i * 0.001) + i * 4 / 5
     radius = 50 + i * 10
     stroke(255 - i * 20, 150, 255, 50)
     beginShape
-    (0..TWO_PI).step(PI / 6) do |a|
+    # (0...TWO_PI).step(PI / 6) do |a|
+    (0...4 * 2).step(4 / 6) do |a|
       x = cos(a + angle) * radius
       y = sin(a + angle) * radius
       vertex(x, y)
