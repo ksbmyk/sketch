b0e9f14843834395871ca0086a8001f2a70aecb1
Miyuki Koshiba <ksbmyk@gmail.com>
2025-01-17 19:37:49 +0900

    change color

--- public/programs/20250117.rb
+++ public/programs/20250117.rb
@@ -1,16 +1,18 @@
 def setup
   createCanvas(600, 600)
   noFill
-  strokeWeight(2)
+  strokeWeight(1)
+  blendMode(ADD)
 end
 
 def draw
-  background(20)
+  background(0)
   translate(width / 2, height / 2)
+
   10.times do |i|
     angle = frameCount * (0.01 + i * 0.001) + i * PI / 5
     radius = 50 + i * 10
-    stroke(255 - i * 20)
+    stroke(255 - i * 20, 150, 255, 50)
     beginShape
     (0..TWO_PI).step(PI / 6) do |a|
       x = cos(a + angle) * radius
