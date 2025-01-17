b1472158c0f4cbf3138c1621925393265ed2dcf4
ksbmyk <ksbmyk@users.noreply.github.com>
2025-01-17 14:18:30 +0000

    deploy: 854ec825efdc06c2b1cdc4e90df876b7178c585b

---
+++ programs/20250117.rb
@@ -0,0 +1,25 @@
+# GENUARY 2025 jan17 "What happens if pi=4?"
+# https://genuary.art/prompts
+
+def setup
+  createCanvas(600, 600)
+  noFill
+end
+
+def draw
+  translate(width / 2, height / 2)
+  # p = PI
+  p = 4
+  10.times do |i|
+    angle = frameCount * (0.01 + i * 0.001) + i * p / 5
+    radius = 50 + i * 10
+    stroke(255 - i * 20, 150, 255, 50)
+    beginShape
+     (0...p * 2).step(p.fdiv(6)) do |a|
+      x = cos(a + angle) * radius
+      y = sin(a + angle) * radius
+      vertex(x, y)
+    end
+    endShape(CLOSE)
+  end
+end
