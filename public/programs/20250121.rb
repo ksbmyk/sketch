4b06ae4d861c21e824e72faae1a5612de1157fa8
Miyuki Koshiba <ksbmyk@gmail.com>
2025-01-21 21:42:04 +0900

    パラメータ調整

--- public/programs/20250121.rb
+++ public/programs/20250121.rb
@@ -1,19 +1,18 @@
 # GENUARY 2025 jan21 "Create a collision detection system (no libraries allowed)."
 # https://genuary.art/prompts
 
-
 def setup
   createCanvas(600, 600)
   
-  circle_count = 10
+  circle_count = 40
   @circles = []
   circle_count.times do
     @circles << {
       x: rand(0..width),
       y: rand(0..height),
-      diameter: rand(20..50),
-      speed_x: rand(-1..1) + 0.1,
-      speed_y: rand(-1..1) + 0.1,
+      diameter: rand(10..30),
+      speed_x: rand(-3..3) + 0.1,
+      speed_y: rand(-3..3) + 0.1,
     }
   end
 end
@@ -22,7 +21,7 @@ def draw
   background(0)
   
   # 線を描画
-  stroke("#ffffff")
+  stroke(255)
   strokeWeight(1)
   @circles.combination(2).each do |circle1, circle2|
     dx = circle1[:x] - circle2[:x]
@@ -60,8 +59,8 @@ def draw
 
           target_x = other[:x] + cos(angle) * min_dist # 衝突時の目標x座標を計算
           target_y = other[:y] + sin(angle) * min_dist # 衝突時の目標y座標を計算
-          ax = (target_x - circle[:x]) * 0.1 # 加速度（x方向）を計算
-          ay = (target_y - circle[:y]) * 0.1 # 加速度（y方向）を計算
+          ax = (target_x - circle[:x]) * 0.5 # 加速度（x方向）を計算
+          ay = (target_y - circle[:y]) * 0.5 # 加速度（y方向）を計算
           circle[:speed_x] += ax # x方向の速度を更新
           circle[:speed_y] += ay # y方向の速度を更新
         end
@@ -84,7 +83,7 @@ def draw
     end
 
     # 速度上限を設けてどんどん速くならないようにする
-    max_speed = 2
+    max_speed = 3
     # 円の現在の速度の大きさを計算
     # sqrt 平方根 速度の大きさ（ベクトルの長さ）をとる
     speed_magnitude = sqrt(circle[:speed_x] ** 2 + circle[:speed_y] ** 2)
@@ -94,7 +93,7 @@ def draw
       circle[:speed_y] *= ratio
     end
 
-    fill("#32f8fd")
+    fill(255)
     noStroke
     ellipse(circle[:x], circle[:y], circle[:diameter])
   end
