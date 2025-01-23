b2a559fcb329be426ef791d66b099f99b1e4fa27
ksbmyk <ksbmyk@users.noreply.github.com>
2025-01-23 11:57:13 +0000

    deploy: 3fc91244ae66d283d5659e36a4d95e6c2f164aba

---
+++ programs/20250123.rb
@@ -0,0 +1,24 @@
+# GENUARY 2025 jan23 "Inspired by brutalism."
+# https://genuary.art/prompts
+
+def setup
+  createCanvas(600, 600)
+  noLoop
+end
+
+def draw
+  background(20)
+  size = 60
+  (0..height).step(size) do |y|
+    (0..width).step(size) do |x|
+      stroke(200)
+      fill(rand(100..255))
+      rect(x, y, size, size)
+      fill(rand(50..200))
+      stroke(rand(50..200))
+      if rand > 0.5
+        ellipse(x + size / 2, y + size / 2, size / 2, size / 2)
+      end
+    end
+  end
+end
