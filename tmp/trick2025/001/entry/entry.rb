e5cd08967b1d3a1a7a47f667c9b098188c557d2d
Miyuki Koshiba <ksbmyk@gmail.com>
2025-02-16 22:47:24 +0900

    add 水平線

--- tmp/trick2025/001/entry/entry.rb
+++ tmp/trick2025/001/entry/entry.rb
@@ -2,4 +2,5 @@
   c=[' ', '.', '*', '✨']
   h.times { |y| system('clear'); y.times { |l| puts ' ' * (l % w) + c.sample }; puts ' ' * (y % w) + '🌟'; sleep s }
   system('clear'); h.times { |l| puts ' ' * (l % w) + c.sample }
+  puts '-' * w
 }[50, 20, 0.1]
