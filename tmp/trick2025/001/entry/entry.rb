77c0e5e0c01474032c01bfd03fc3705af6397636
Miyuki Koshiba <ksbmyk@gmail.com>
2025-02-19 21:37:52 +0900

    delete space

--- tmp/trick2025/001/entry/entry.rb
+++ tmp/trick2025/001/entry/entry.rb
@@ -1,5 +1,5 @@
 ->(w, h, s) {
-  c=[' ', '.', '*', '✨']
-  h.times { |y| system('clear'); y.times { |l| puts ' '.rjust(l % w) + c.sample }; puts '🌟'.rjust(y % w); sleep s }
-  system('clear'); h.times { |l| puts ' '.rjust(l % w) + c.sample };puts '⛰️ ' * h
-}[50, 20, 0.1]
+  c=[' ','.','*','✨']
+  h.times{|y|system('clear');y.times{|l|puts' '.rjust(l%w)+c.sample};puts'🌟'.rjust(y%w);sleep s}
+  system('clear');h.times{|l|puts' '.rjust(l%w)+c.sample};puts'⛰️ '*h
+}[50,20,0.1]
