7ebe454e346845050b2698c611793b3297decfef
Miyuki Koshiba <ksbmyk@gmail.com>
2025-02-19 21:34:04 +0900

    use rjust

--- tmp/trick2025/001/entry/entry.rb
+++ tmp/trick2025/001/entry/entry.rb
@@ -1,6 +1,5 @@
 ->(w, h, s) {
   c=[' ', '.', '*', '✨']
-  h.times { |y| system('clear'); y.times { |l| puts ' ' * (l % w) + c.sample }; puts ' ' * (y % w) + '🌟'; sleep s }
-  system('clear'); h.times { |l| puts ' ' * (l % w) + c.sample }
-  puts '⛰️ ' * h
+  h.times { |y| system('clear'); y.times { |l| puts ' '.rjust(l % w) + c.sample }; puts '🌟'.rjust(y % w); sleep s }
+  system('clear'); h.times { |l| puts ' '.rjust(l % w) + c.sample };puts '⛰️ ' * h
 }[50, 20, 0.1]
