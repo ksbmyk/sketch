7e46e5967255b3da2bdc8639d22b1ffe1207483f
Miyuki Koshiba <ksbmyk@gmail.com>
2025-02-19 21:54:41 +0900

    use 32.chr

--- tmp/trick2025/001/entry/entry.rb
+++ tmp/trick2025/001/entry/entry.rb
@@ -1,5 +1,5 @@
 ->(w, h, s) {
-  c=[' ','.','*','✨']
-  h.times{|y|print "\e[H\e[2J";y.times{|l|puts' '.rjust(l%w)+c.sample};puts'🌟'.rjust(y%w);sleep s}
-  print "\e[H\e[2J";h.times{|l|puts' '.rjust(l%w)+c.sample};puts'⛰️ '*h
+  c=[32.chr,'.','*','✨']
+  h.times{|y|print"\e[H\e[2J";y.times{|l|puts 32.chr.rjust(l%w)+c.sample};puts'🌟'.rjust(y%w);sleep s}
+  print"\e[H\e[2J";h.times{|l|puts 32.chr.rjust(l%w)+c.sample};puts'⛰️ '*h
 }[50,20,0.1]
