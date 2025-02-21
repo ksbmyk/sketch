77b317d12f45cce6ddcfa16fe61ee8377fc6bf65
Miyuki Koshiba <ksbmyk@gmail.com>
2025-02-21 10:59:35 +0900

    delete space

--- tmp/trick2025/001/entry/entry.rb
+++ tmp/trick2025/001/entry/entry.rb
@@ -1,4 +1,4 @@
-->(w, h, s) {
+->(w,h,s){
   c=[32.chr,'.','*','✨']
   h.times{|y|print"\e[H\e[2J";y.times{|l|puts 32.chr.rjust(l%w)+c.sample};puts'🌟'.rjust(y%w);sleep s}
   print"\e[H\e[2J";h.times{|l|puts 32.chr.rjust(l%w)+c.sample};puts'⛰️ '*h
