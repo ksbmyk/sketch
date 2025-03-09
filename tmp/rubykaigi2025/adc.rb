02c614e1bd6fd97772ffed68ab07ca2fcedf4f41
Miyuki Koshiba <ksbmyk@gmail.com>
2025-03-09 12:51:47 +0900

    トグルスイッチを実装

--- tmp/rubykaigi2025/adc.rb
+++ tmp/rubykaigi2025/adc.rb
@@ -1,15 +1,18 @@
 require 'adc'
 
+# 疎通確認用Lチカ
 pin = GPIO.new(25, GPIO::OUT)
 pin.write 1
 
-adc = ADC.new(26)
-sleep 1 
+# adc = ADC.new(26)
+
+toggle = GPIO.new(2, GPIO::IN|GPIO::PULL_UP) # GP2（物理4）内部プルアップに設定
 
 loop do
-  value = adc.read
-  value_r = adc.read_raw
-  voltage = adc.read_voltage
-  puts "ADC Value: #{value}, #{value_r}, Voltage: #{voltage} V"
+  puts "Toggle Switch State: #{toggle.read}"
+
+  # value = adc.read # 0-3.3
+  # value_r = adc.read_raw # 0-4095
+  # puts "ADC Value: #{value}, #{value_r}"
   sleep 1
 end
