1eeaa279a0941c72263755d153d56eb4a1a39740
Miyuki Koshiba <ksbmyk@gmail.com>
2025-03-09 14:16:16 +0900

    ボリューム3つに対応

--- tmp/rubykaigi2025/adc.rb
+++ tmp/rubykaigi2025/adc.rb
@@ -4,15 +4,34 @@ require 'adc'
 pin = GPIO.new(25, GPIO::OUT)
 pin.write 1
 
-# adc = ADC.new(26)
+# ポテンショメータ
+sensor_pins = [26, 27, 28]  # GP26～GP28
+sensors = sensor_pins.map { |pin| ADC.new(pin) }
 
-toggle = GPIO.new(2, GPIO::IN|GPIO::PULL_UP) # GP2（物理4）内部プルアップに設定
+toggle_pin = 2 # GP2（物理4）
+toggle = GPIO.new(toggle_pin, GPIO::IN|GPIO::PULL_UP) # 内部プルアップに設定
+
+# 初期値
+last_sensor_values = Array.new(sensor_pins.size, 0)
+last_toggle_state = false
 
 loop do
-  puts "Toggle Switch State: #{toggle.read}"
+  toggle_state = toggle.read == 0  # LOW の場合 ON
 
-  # value = adc.read # 0-3.3
-  # value_r = adc.read_raw # 0-4095
-  # puts "ADC Value: #{value}, #{value_r}"
-  sleep 1
+  # トグルスイッチの変化があれば送信
+  if toggle_state != last_toggle_state
+    puts "T,#{toggle_state ? 1 : 0}"
+    last_toggle_state = toggle_state
+  end
+
+  # ポテンショメータの値をチェック
+  sensors.each_with_index do |sensor, i|
+    value = sensor.read_raw # 0〜4095 の値を取得
+    if (value - last_sensor_values[i]).abs > 20 # 変化が大きければ送信
+      puts "#{i},#{value}"
+      last_sensor_values[i] = value
+    end
+  end
+
+  sleep 0.5
 end
