ff9cdcbb3fac31eae992c7625a547947ec683386
Miyuki Koshiba <ksbmyk@gmail.com>
2025-03-12 00:13:51 +0900

    add 可変抵抗器とスイッチの値をPCに送れるようにする

--- tmp/rubykaigi2025/pico_sample.rb
+++ tmp/rubykaigi2025/pico_sample.rb
@@ -5,13 +5,14 @@ require 'adc'
 pin = GPIO.new(25, GPIO::OUT)
 pin.write 1
 
-# uart = UART.new(0)  # UART0 を使用（GP0: TX, GP1: RX）
-uart = UART.new(unit: :RP2040_UART0, txd_pin: 0, rxd_pin: 1, baudrate: 9600)
+# UART0 を使用（GP0: TX, GP1: RX）
+uart = UART.new(unit: :RP2040_UART0, txd_pin: 0, rxd_pin: 1, baudrate: 115200)
 
 # ポテンショメータ
 sensor_pins = [26, 27, 28]  # GP26～GP28
 sensors = sensor_pins.map { |pin| ADC.new(pin) }
 
+# ポテンショメータより先に設定するとトグルスイッチの値がとれない
 toggle_pin = 2 # GP2（物理4）
 toggle = GPIO.new(toggle_pin, GPIO::IN|GPIO::PULL_UP) # 内部プルアップに設定
 
@@ -20,13 +21,16 @@ last_sensor_values = Array.new(sensor_pins.size, 0)
 last_toggle_state = false
 
 loop do
-  toggle_state = toggle.read == 0  # LOW の場合 ON
+  # uart.write("Hello from Pico!\n")
+  # sleep 0.5
 
+  toggle_state = toggle.read == 0  # LOW の場合 ON
   # トグルスイッチの変化があれば送信
   if toggle_state != last_toggle_state
     puts "T,#{toggle_state ? 1 : 0}"
     message = "T,#{toggle_state ? 1 : 0}\n"
     uart.write(message)  # シリアル送信
+    sleep 0.5
     last_toggle_state = toggle_state
   end
 
@@ -37,9 +41,8 @@ loop do
       puts "#{i},#{value}"
       message = "#{i},#{value}\n"
       uart.write(message)  # シリアル送信
+      sleep 0.5
       last_sensor_values[i] = value
     end
   end
-
-  sleep 0.5
 end
