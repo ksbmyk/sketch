6d9221fbba7d6417fa401677a3f30e12308bb49b
Miyuki Koshiba <ksbmyk@gmail.com>
2025-03-13 22:56:29 +0900

    ポテンショメータ3つに対応

--- tmp/rubykaigi2025/pico_sample.rb
+++ tmp/rubykaigi2025/pico_sample.rb
@@ -6,17 +6,15 @@ pin = GPIO.new(25, GPIO::OUT)
 pin.write 1
 
 # ポテンショメータ
-# sensor_pins = [26, 27, 28]  # GP26～GP28
-# sensors = sensor_pins.map { |pin| ADC.new(pin) }
-sensor = ADC.new(26)
+sensor_pins = [26, 27, 28]  # GP26～GP28
+sensors = sensor_pins.map { |pin| ADC.new(pin) }
 
 # ポテンショメータより先に設定するとトグルスイッチの値がとれない
 toggle_pin = 2 # GP2（物理4）
 toggle = GPIO.new(toggle_pin, GPIO::IN|GPIO::PULL_UP) # 内部プルアップに設定
 
 # 初期値
-# last_sensor_values = Array.new(sensor_pins.size, 0)
-last_sensor_value = 0
+last_sensor_values = Array.new(sensor_pins.size, 0)
 last_toggle_state = false
 
 # UART0 を使用（GP0: TX, GP1: RX）
@@ -37,27 +35,16 @@ loop do
   end
 
   # ポテンショメータの値をチェック
-  # sensors.each_with_index do |sensor, i|
-  #   if (Time.now.to_f.to_i % 2) == 0 # 2秒に1回
-  #     value = sensor.read_raw # 0〜4095 の値を取得
-  #     if (value - last_sensor_values[i]).abs > 20 # 変化が大きければ送信
-  #       puts "#{i},#{value}"
-  #       message = "#{i},#{value}\n"
-  #       uart.write(message)  # シリアル送信
-  #       sleep 0.5
-  #       last_sensor_values[i] = value
-  #     end
-  #   end
-  # end
-
-  if Time.now.to_i % 2 == 0 # 2秒に1回
-    value = sensor.read_raw # 0〜4095 の値を取得
-    if (value - last_sensor_value).abs > 20 # 変化が大きければ送信
-      puts "0,#{value}"
-      message = "0,#{value}\n"
-      uart.write(message)  # シリアル送信
-      sleep 0.5
-      last_sensor_value = value
+  sensors.each_with_index do |sensor, i|
+    if (Time.now.to_f.to_i % 2) == 0 # 2秒に1回
+      value = sensor.read_raw # 0〜4095 の値を取得
+      if (value - last_sensor_values[i]).abs > 20 # 変化が大きければ送信
+        puts "#{i},#{value}"
+        message = "#{i},#{value}\n"
+        uart.write(message)  # シリアル送信
+        sleep 0.5
+        last_sensor_values[i] = value
+      end
     end
   end
 end
