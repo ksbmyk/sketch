bc2d4907a1c7b9939731a0ae7ea3cd1ccb726acd
Miyuki Koshiba <ksbmyk@gmail.com>
2025-03-20 18:02:53 +0900

    use processing

--- tmp/rubykaigi2025/pc.rb
+++ tmp/rubykaigi2025/pc.rb
@@ -1,21 +1,22 @@
 require "uart"
+require "processing"
+using Processing
 
-# ポートを指定 (Mac ls /dev/cu.* で確認)
-# Arduino の場合
-# port = "/dev/cu.usbmodem1101"
-# baud = 9600
-# Raspberry Pi Pico
-port = "/dev/cu.usbserial-BG00ECUT"
-baud = 115200
+def setup
+  # ポートを指定 (Mac ls /dev/cu.* で確認)
+  # Arduino の場合
+  # port = "/dev/cu.usbmodem1101"
+  # baud = 9600
+  # Raspberry Pi Pico
+  port = "/dev/cu.usbserial-BG00ECUT"
+  baud = 115200
 
-# UART 通信を開始
-uart = UART.open(port, baud)
+  # UART 通信を開始
+  @uart = UART.open(port, baud)
+end
 
-puts "pico からのデータを待機中..."
-
-# 継続的にデータを受信
-loop do
-  data = uart.gets
+def draw
+  data = @uart.gets
 
   # return if data.nil? || data.empty?
   if data
