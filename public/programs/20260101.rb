5d51bac8a579fc14cc3881eb9934bd966ac593b9
Miyuki Koshiba <ksbmyk@gmail.com>
2026-01-01 14:31:26 +0900

    線の太さを動的に、色のグラデーションをつける

--- public/programs/20260101.rb
+++ public/programs/20260101.rb
@@ -1,10 +1,8 @@
 # GENUARY 2026 jan1 "One color, one shape."
 # https://genuary.art/prompts
-
 def setup
   createCanvas(windowWidth, windowWidth)
   colorMode(HSB, 360, 100, 100, 255)
-  #noStroke
   @angle_offset = 0
 end
 
@@ -20,14 +18,13 @@ def draw
   end
 
   translate(width / 2, height / 2)
-  @hue_value = 100
 
   @alpha_value = 150
-
-  stroke(@hue_value, 80, 100, @alpha_value)
-
   @speed = 0.05
   @angle_offset += @speed
+
+  # 時間で色相を変化（0〜360をループ）
+  @base_hue = (@angle_offset * 20) % 360
 
   @circle_count = 10
 
@@ -37,8 +34,16 @@ def draw
 
     x = cos(angle) * @distance
     y = sin(angle) * @distance
-    @circle_size = 150
 
+    # 各円で色相をずらしてグラデーション
+    hue = (@base_hue + i * 15) % 360
+    stroke(hue, 80, 100, @alpha_value)
+
+    # strokeWeightを動的に変化（1〜6の範囲）
+    weight = 3 + sin(@angle_offset + i * 0.5) * 2
+    strokeWeight(weight)
+
+    @circle_size = 150
     circle(x, y, @circle_size + (i.even? ? 30 : -30))
   end
 end
