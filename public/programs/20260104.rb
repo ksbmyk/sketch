add11a992f87652e8c944761ae7a99f9ad635ef8
Miyuki Koshiba <ksbmyk@gmail.com>
2026-01-04 20:12:38 +0900

    リサージュ曲線

---
+++ public/programs/20260104.rb
@@ -0,0 +1,29 @@
+def setup
+  createCanvas(700, 700)
+  colorMode(HSB, 360, 100, 100, 100)
+  noFill
+  stroke(190, 80, 100)
+  strokeWeight(2)
+end
+
+def draw
+  background(0)
+  translate(width / 2, height / 2)
+  
+  a = 3
+  b = 4
+  
+  margin = 70                                 # 画面端からの余白
+  amplitude = width / 2 - margin              # 振幅
+  angle_step = 0.5                            # 角度の刻み（度）
+  num_points = (360 / angle_step).to_i + 1    # 点の数
+  
+  beginShape
+  num_points.times do |i|
+    angle = radians(i * angle_step)
+    x = amplitude * sin(a * angle)
+    y = amplitude * sin(b * angle)
+    vertex(x, y)
+  end
+  endShape
+end
