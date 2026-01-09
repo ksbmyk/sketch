aec779824723081920a6805bb4bcee49be2dad82
Miyuki Koshiba <ksbmyk@gmail.com>
2026-01-09 23:44:53 +0900

    add comments

--- public/programs/20260109.rb
+++ public/programs/20260109.rb
@@ -1,3 +1,6 @@
+# GENUARY 2026 jan9 "Crazy automaton. Cellular automata with crazy rules."
+# https://genuary.art/prompts
+
 def setup
   createCanvas(700, 700)
   colorMode(HSB, 360, 100, 100, 100)
