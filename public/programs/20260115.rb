9135e2d0c88b99a41f3c741ec24f1fd4d2099d69
Miyuki Koshiba <ksbmyk@gmail.com>
2026-01-12 18:08:13 +0900

    refactor

--- public/programs/20260115.rb
+++ public/programs/20260115.rb
@@ -7,10 +7,15 @@ def setup
   createCanvas(700, 700)
   colorMode(HSB, 360, 100, 100, 100)
   
+  @silhouette = build_cube_silhouette
+  @center_x = width / 2.0
+  @center_y = height / 2.0
+end
+
+def build_cube_silhouette
   s = CUBE_SIZE
   cx = width / 2.0
   cy = height / 2.0
-  
   rot_x = 0.4
   rot_y = 0.6
   
@@ -19,43 +24,40 @@ def setup
     [-s, s, -s], [s, s, -s], [s, s, s], [-s, s, s]
   ]
   
-  cube_2d = base.map do |v|
-    x1 = v[0] * cos(rot_y) - v[2] * sin(rot_y)
-    z1 = v[0] * sin(rot_y) + v[2] * cos(rot_y)
-    y1 = v[1]
-    
-    y2 = y1 * cos(rot_x) - z1 * sin(rot_x)
+  cube_2d = base.map do |x, y, z|
+    x1 = x * cos(rot_y) - z * sin(rot_y)
+    z1 = x * sin(rot_y) + z * cos(rot_y)
+    y2 = y * cos(rot_x) - z1 * sin(rot_x)
     
     { x: x1 + cx, y: y2 + cy }
   end
   
-  @silhouette = convex_hull(cube_2d)
-  @center_x = width / 2.0
-  @center_y = height / 2.0
+  convex_hull(cube_2d)
 end
 
 def convex_hull(points)
   sorted = points.sort_by { |p| [p[:y], p[:x]] }
   start = sorted.first
   
-  rest = sorted[1..-1].sort_by do |p|
+  rest = sorted.drop(1).sort_by do |p|
     atan2(p[:y] - start[:y], p[:x] - start[:x])
   end
   
-  hull = [start]
-  
-  rest.each do |p|
-    while hull.length > 1 && cross(hull[-2], hull[-1], p) <= 0
-      hull.pop
-    end
+  rest.each_with_object([start]) do |p, hull|
+    hull.pop while hull.size > 1 && cross(hull[-2], hull[-1], p) <= 0
     hull << p
   end
-  
-  hull
 end
 
 def cross(o, a, b)
   (a[:x] - o[:x]) * (b[:y] - o[:y]) - (a[:y] - o[:y]) * (b[:x] - o[:x])
+end
+
+def silhouette_edges
+  @silhouette.map.with_index do |v1, j|
+    v2 = @silhouette[(j + 1) % @silhouette.size]
+    [v1, v2]
+  end
 end
 
 def draw
@@ -65,77 +67,50 @@ def draw
   angle2 = angle1 + PI
   
   lights = [
-    create_light(angle1, 190),  # cyan-blue
-    create_light(angle2, 240)   # blue-purple
+    build_light(angle1, 190),
+    build_light(angle2, 240)
   ]
   
   blendMode(ADD)
-  
-  lights.each do |light|
-    draw_rays(light)
-  end
+  lights.each { |light| draw_rays(light) }
   
   blendMode(BLEND)
-  
-  lights.each do |light|
-    draw_light_indicator(light[:x], light[:y], light[:hue])
-  end
+  lights.each { |light| draw_light_indicator(light) }
 end
 
-def create_light(orbit_angle, hue)
+def build_light(orbit_angle, hue)
   x = @center_x + cos(orbit_angle) * ORBIT_RADIUS
   y = @center_y + sin(orbit_angle) * ORBIT_RADIUS
-  
   angle_to_center = atan2(@center_y - y, @center_x - x)
   
-  {
-    x: x,
-    y: y,
-    hue: hue,
-    angle_center: angle_to_center,
-    angle_spread: PI / 2
-  }
+  { x: x, y: y, hue: hue, angle_center: angle_to_center, angle_spread: PI / 2 }
 end
 
 def draw_rays(light)
-  light_x = light[:x]
-  light_y = light[:y]
-  hue = light[:hue]
-  angle_center = light[:angle_center]
-  angle_spread = light[:angle_spread]
-  
-  start_angle = angle_center - angle_spread / 2
+  start_angle = light[:angle_center] - light[:angle_spread] / 2
   
   NUM_RAYS.times do |i|
-    ray_angle = start_angle + (i.to_f / NUM_RAYS) * angle_spread
-    
-    dx = cos(ray_angle)
-    dy = sin(ray_angle)
-    
-    hit_t = nil
-    
-    @silhouette.length.times do |j|
-      v1 = @silhouette[j]
-      v2 = @silhouette[(j + 1) % @silhouette.length]
-      
-      t = ray_segment_intersection(light_x, light_y, dx, dy, v1[:x], v1[:y], v2[:x], v2[:y])
-      if t && t > 5
-        hit_t = t if hit_t.nil? || t < hit_t
-      end
-    end
-    
-    if hit_t
-      end_x = light_x + dx * hit_t
-      end_y = light_y + dy * hit_t
-    else
-      end_x = light_x + dx * RAY_LENGTH
-      end_y = light_y + dy * RAY_LENGTH
-    end
-    
-    stroke(hue, 80, 80, 35)
-    strokeWeight(1.5)
-    line(light_x, light_y, end_x, end_y)
+    ray_angle = start_angle + (i.to_f / NUM_RAYS) * light[:angle_spread]
+    draw_single_ray(light[:x], light[:y], ray_angle, light[:hue])
   end
+end
+
+def draw_single_ray(light_x, light_y, ray_angle, hue)
+  dx = cos(ray_angle)
+  dy = sin(ray_angle)
+
+  hit_distances = silhouette_edges.map do |v1, v2|
+    ray_segment_intersection(light_x, light_y, dx, dy, v1[:x], v1[:y], v2[:x], v2[:y])
+  end
+
+  hit_t = hit_distances.compact.select { |t| t > 5 }.min || RAY_LENGTH
+
+  end_x = light_x + dx * hit_t
+  end_y = light_y + dy * hit_t
+
+  stroke(hue, 80, 80, 35)
+  strokeWeight(1.5)
+  line(light_x, light_y, end_x, end_y)
 end
 
 def ray_segment_intersection(rx, ry, dx, dy, x1, y1, x2, y2)
@@ -148,15 +123,15 @@ def ray_segment_intersection(rx, ry, dx, dy, x1, y1, x2, y2)
   t = ((x1 - rx) * seg_dy - (y1 - ry) * seg_dx) / denom
   s = ((x1 - rx) * dy - (y1 - ry) * dx) / denom
   
-  (t > 0 && s >= 0 && s <= 1) ? t : nil
+  t > 0 && s >= 0 && s <= 1 ? t : nil
 end
 
-def draw_light_indicator(x, y, hue)
+def draw_light_indicator(light)
   noStroke
   5.downto(1) do |i|
-    fill(hue, 60, 95, 6 * i)
-    circle(x, y, i * 14)
+    fill(light[:hue], 60, 95, 6 * i)
+    circle(light[:x], light[:y], i * 14)
   end
-  fill(hue, 40, 100, 95)
-  circle(x, y, 8)
+  fill(light[:hue], 40, 100, 95)
+  circle(light[:x], light[:y], 8)
 end
